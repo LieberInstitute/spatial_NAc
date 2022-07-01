59ebd57839f5130924f16d1a4a02d7ffce795fcf
heenadivecha <heenadivecha@gmail.com>
2022-07-01 13:31:56 -0400

    Edited the script to run splitslide for round 1 image (Br8325)

--- code/VistoSeg/code/splitSlide.sh
+++ code/VistoSeg/code/splitSlide.sh
@@ -2,8 +2,8 @@
 #$ -cwd
 #$ -l mem_free=20G,h_vmem=20G,h_fsize=100G
 #$ -pe local 7
-#$ -o /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/logs/03-28-2022_Visium_V11U08-081_HPC_Round8.txt
-#$ -e /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/logs/03-28-2022_Visium_V11U08-081_HPC_Round8.txt
+#$ -o logs/06-02-2022_V11U08-082_Br8325_Round1_splitSlide.txt
+#$ -e logs/06-02-2022_V11U08-082_Br8325_Round1_splitSlide.txt 
 #$ -m e
 #$ -M heenadivecha@gmail.com
 #$ -t 1
@@ -21,16 +21,16 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 echo "****"
-echo "Sample id: 03-28-2022_Visium_V11U08-081_HPC_Round8"
+echo "Sample id: 06-02-2022_V11U08-082_Br8325_Round1"
 echo "****"
 
 module load matlab/R2019a
 
-toolbox='/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code'
-fname='/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/raw-data/Images/CS2/round8/03-28-2022_Visium_V11U08-081_HPC_Round8.tif'
+toolbox='/dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/code/VistoSeg/code'
+fname='/dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/raw-data/images/CS2/round1/06-02-2022_V11U08-082_Br8325_Round1.tif'
 
 
-matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide('$fname',90,90,0,0)"
+matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide('$fname',180,0,0,0)"
 
 echo "**** Job ends ****"
 date
