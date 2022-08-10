7224a28a4c709108cb747be63184349000713f41
heenadivecha <heenadivecha@gmail.com>
2022-08-10 15:25:47 -0400

    Version controlling VNS.sh script for Br8325_Round1_A1

--- code/VistoSeg/code/VNS.sh
+++ code/VistoSeg/code/VNS.sh
@@ -2,8 +2,8 @@
 #$ -cwd
 #$ -l mem_free=20G,h_vmem=20G,h_fsize=100G
 #$ -pe local 10
-#$ -o logs/06-02-2022_V11U08-082_Br8325_Round1_VNS.txt
-#$ -e logs/06-02-2022_V11U08-082_Br8325_Round1_VNS.txt 
+#$ -o logs/06-02-2022_V11U08-082_Br8325_Round1_A1_VNS.txt
+#$ -e logs/06-02-2022_V11U08-082_Br8325_Round1_A1_VNS.txt 
 #$ -m e
 #$ -M heenadivecha@gmail.com
 #$ -t 1
@@ -21,13 +21,13 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 echo "****"
-echo "Sample id: 06-02-2022_V11U08-082_Br8325_Round1"
+echo "Sample id: 06-02-2022_V11U08-082_Br8325_Round1_A1"
 echo "****"
 
 module load matlab/R2019a
 
 toolbox='/dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/code/VistoSeg/code'
-fname='/dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/raw-data/images/CS2/round1/06-02-2022_V11U08-082_Br8325_Round1.tif'
+fname='/dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/raw-data/images/CS2/round1/06-02-2022_V11U08-082_Br8325_Round1_A1.tif'
 
 
 matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), VNS('$fname',5)"
