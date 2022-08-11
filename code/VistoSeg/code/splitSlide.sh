43aeeacc02d440ddc9df6c9534cc0a6cddbfcf95
heenadivecha <heenadivecha@gmail.com>
2022-08-11 13:31:35 -0400

    Version controlling the script and log file for Br6522_Round3(splitSlide)

--- code/VistoSeg/code/splitSlide.sh
+++ code/VistoSeg/code/splitSlide.sh
@@ -2,8 +2,8 @@
 #$ -cwd
 #$ -l mem_free=20G,h_vmem=20G,h_fsize=100G
 #$ -pe local 8
-#$ -o logs/07-27-2022_V11U23-403_Br6423_Round3_splitSlide.txt
-#$ -e logs/07-27-2022_V11U23-403_Br6423_Round3_splitSlide.txt 
+#$ -o logs/07-27-2022_V11U23-405_Br6522_Round3_splitSlide.txt
+#$ -e logs/07-27-2022_V11U23-405_Br6522_Round3_splitSlide.txt 
 #$ -m e
 #$ -M heenadivecha@gmail.com
 #$ -t 1
@@ -21,13 +21,13 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 echo "****"
-echo "Sample id: 07-27-2022_V11U23-403_Br6423_Round3"
+echo "Sample id: 07-27-2022_V11U23-405_Br6522_Round3"
 echo "****"
 
 module load matlab/R2019a
 
 toolbox='/dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/code/VistoSeg/code'
-fname='/dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/raw-data/images/CS2/round3/07-27-2022_V11U23-403_Br6423_Round3.tif'
+fname='/dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/raw-data/images/CS2/round3/07-27-2022_V11U23-405_Br6522_Round3.tif'
 
 
 matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide('$fname',0,0,0,0)"
