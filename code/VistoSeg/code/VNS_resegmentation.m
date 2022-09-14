e51779925d3501443a18e1194a97346a8c62c2a7
heenadivecha <heenadivecha@gmail.com>
2022-09-14 13:09:24 -0400

    Add the script to get more refined segmentaions after apply the size threshold

--- code/VistoSeg/code/VNS_resegmentation.m
+++ code/VistoSeg/code/VNS_resegmentation.m
@@ -1,37 +1,4 @@
-function VNS(fname,N,n)
-%fname = '/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/spatialDLPFC/Images/Lieber_Institute_OTS-20-7748_rush_posterior_A1.tif
-%N = 5;
-
-tic
-disp('Importing capture area')
-Img1 = imread(fname); %import image
-toc
-
-he = Img1 %based on VNS.m and there is no contrast adjustment here
-
-tic
-disp('Performing rgb to Lab color space conversion')
-lab_he = rgb2lab(he); % convert from rgb color space to Lab color space
-toc
-ab = lab_he(:,:,2:3); % extract a*b color space from Lab
-ab = im2single(ab);
-tic
-disp('Applying Kmeans')
-pixel_labels = imsegkmeans(ab,N,'NumAttempts',3); % apply Kmeans
-toc
-
-tic
-disp('saving outputs')
-parfor i = 1:N
-mask{i} = pixel_labels==i;
-cluster{i} = he .* uint8(mask{i});
-imwrite(cluster{i},[fname(1:end-4),'_cluster',num2str(i),'.tif'])
-end
-
-save([fname(1:end-4),'_mask.mat'],'mask','-v7.3')
-save([fname(1:end-4),'_cluster.mat'],'cluster','-v7.3')
-toc 
-
-load('*mask.mat')
-BW = bwarefilt(mask{n},[20 2000]); % 'n' will the cluster number associated with nuclei from 2nd run
+%fname = 'nuclei.mat';
+load(fname)
+BW = bwareafilt(mask_dark_blue,[20 2000]); % 'n' will the cluster number associated with nuclei from 2nd run
 save('*nuclei_final.mat','BW')
