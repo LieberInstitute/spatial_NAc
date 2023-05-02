93849b3a038128c3b539371088afd96a9cdd8d18
Nick-Eagles <nick.eagles@libd.org>
2023-05-02 15:35:43 -0400

    Debug stitching script after having access to JHPCE. Use actual paths and images for the 4 samples rather than dummy images

--- code/02_image_stitching/01-samui_test.py
+++ code/02_image_stitching/01-samui_test.py
@@ -5,20 +5,23 @@ import session_info
 import numpy as np
 import pandas as pd
 import json
+import os
 from loopy.sample import Sample
+import tifffile
 
-img_paths = []
-
-sample_info_path = here('processed-data', '02_image_stitching', 'sample_info_clean.csv')
+sample_info_path = here(
+    'processed-data', '02_image_stitching', 'sample_info_clean.csv'
+)
 out_dir = here('processed-data', '02_image_stitching', 'combined_Br8325')
 
-spot_diameter_m = 55e-6 # 55-micrometer diameter for Visium spot
+#   55-micrometer diameter for Visium spot
+SPOT_DIAMETER_M = 55e-6
 
 #   Image dimensions, which should match between images
-EXPECTED_SHAPE = (100, 100, 3)
+EXPECTED_SHAPE = (31286, 25515, 3)
 
 #   Array defining an affine transformation:
-#   [x', y'] = trans[0] @ [x, y, 1]
+#   [x0', y0'] = trans[0] @ [x0, y0, 1]
 trans = np.array(
     [
         [[1, 0, 1365], [0, 1, 18]],
@@ -31,36 +34,52 @@ trans = np.array(
 
 Path(out_dir).mkdir(parents = True, exist_ok = True)
 
+#   Read in sample info and subset to the samples of interest
 sample_info = pd.read_csv(sample_info_path, index_col = 0)
-sample_info.query('Brain == "Br8325"')
+sample_info = sample_info.loc[
+    (sample_info['Brain'] == "Br8325") &
+    (sample_info['Sample #'] != "32v_svb"),
+    :
+]
 
-#   Will be retrieved from spaceranger JSON
-SCALEFACTOR = 20
+#   Loop through all samples and rescale translations to be at full resolutions
+#   (the initial definition of 'trans' uses high-resolution values)
+for i in range(sample_info.shape[0]):
+    #   Grab high-res scale factor and scale translations accordingly
+    json_path = os.path.join(
+        sample_info['spaceranger_dir'].iloc[i], 'scalefactors_json.json'
+    )
+    with open(json_path, 'r') as f:
+        spaceranger_json = json.load(f)
+    
+    trans[i, :, 2] /= spaceranger_json['tissue_hires_scalef']
 
-#   Scale translations to full resolution (values were provided in high
-#   resolution)
-trans[:, :, 2] /= SCALEFACTOR
+#   Translations must be an integer number of pixels
+trans[:, :, 2] = np.round(trans[:, :, 2])
 
 #   Initialize the combined tiff, for now using floats (we're averaging pixels
-#   across several images before fixing the data type)
-max0 = EXPECTED_SHAPE[0] + np.max(trans[:, 0])
-max1 = EXPECTED_SHAPE[1] + np.max(trans[:, 1])
-combined_img = np.zeros((max0, max1, 3), dtype = np.float64)
-weights = np.zeros((max0, max1, 3), dtype = np.float64)
+#   across several images before fixing the data type). 16 bits to save memory
+max0 = int(EXPECTED_SHAPE[0] + np.max(trans[:, 0, 2]))
+max1 = int(EXPECTED_SHAPE[1] + np.max(trans[:, 1, 2]))
+combined_img = np.zeros((max0, max1, 3), dtype = np.float16)
+weights = np.zeros((max0, max1, 3), dtype = np.float16)
 
 #   Define example RGB images just for testing
-imgs = [np.zeros(EXPECTED_SHAPE, dtype = np.uint8) for x in range(4)]
-imgs[0][:, :, 0] = 255
-imgs[1][:, :, 1] = 255
-imgs[2][:, :, 2] = 255
-imgs[3][:, :, :2] = 127
+# imgs = [np.zeros(EXPECTED_SHAPE, dtype = np.uint8) for x in range(4)]
+# imgs[0][:, :, 0] = 255
+# imgs[1][:, :, 1] = 255
+# imgs[2][:, :, 2] = 255
+# imgs[3][:, :, :2] = 127
 
 #   For now, just read in one JSON file and assume all samples are roughly on
 #   the same spatial scale (spots have the same diameter in pixels)
+json_path = os.path.join(
+    sample_info['spaceranger_dir'].iloc[0], 'scalefactors_json.json'
+)
 with open(json_path, 'r') as f:
     spaceranger_json = json.load(f)
 
-m_per_px = spot_diameter_m / spaceranger_json['spot_diameter_fullres']
+m_per_px = SPOT_DIAMETER_M / spaceranger_json['spot_diameter_fullres']
 
 tissue_positions_list = []
 
@@ -68,13 +87,15 @@ tissue_positions_list = []
 #   Read in and translate images and spot coordinates
 ################################################################################
 
-for i in range(4):
-    img = imgs[i]
-
-    #   Define "sample_id"; "tissue_path" for this sample
+for i in range(sample_info.shape[0]):
+    img = tifffile.imread(sample_info['raw_image_path'].iloc[i])
+    assert img.shape == EXPECTED_SHAPE, "Image is not of expected dimensions."
 
     #   Read in the tissue positions file to get spatial coordinates. Index by
     #   barcode + sample ID, and subset to only spots within tissue
+    tissue_path = os.path.join(
+        sample_info['spaceranger_dir'].iloc[i], 'tissue_positions_list.csv'
+    )
     tissue_positions = pd.read_csv(
             tissue_path,
             header = None,
@@ -82,25 +103,27 @@ for i in range(4):
             names = ["in_tissue", "row", "col", "y", "x"],
             index_col = 0
         ).query('in_tissue == 1')
-    tissue_positions.index = tissue_positions.index + sample_id
+    tissue_positions.index = tissue_positions.index + '_' + sample_info.index[i]
 
     #   Apply affine transform of coordinates
-    tissue_positions[['x', 'y']] = trans[i] @ \
-        np.array(tissue_positions.assign(ones = 1)[['x', 'y', 'ones']])
+    tissue_positions[['x', 'y']] = (
+        trans[i] @ 
+        np.array(tissue_positions.assign(ones = 1)[['x', 'y', 'ones']]).T
+    ).T
 
     tissue_positions_list.append(tissue_positions)
 
     #   "Place this image" on the combined image, considering translations
     combined_img[
-            trans[i][0]: trans[i][0] + EXPECTED_SHAPE[0],
-            trans[i][1]: trans[i][1] + EXPECTED_SHAPE[1],
+            int(trans[i, 0, 2]): int(trans[i, 0, 2] + EXPECTED_SHAPE[0]),
+            int(trans[i, 1, 2]): int(trans[i, 1, 2] + EXPECTED_SHAPE[1]),
             :
         ] += img
 
     #   Keep track of how many times each pixel was added to
     weights[
-            trans[i][0]: trans[i][0] + EXPECTED_SHAPE[0],
-            trans[i][1]: trans[i][1] + EXPECTED_SHAPE[1],
+            int(trans[i, 0, 2]): int(trans[i, 0, 2] + EXPECTED_SHAPE[0]),
+            int(trans[i, 1, 2]): int(trans[i, 1, 2] + EXPECTED_SHAPE[1]),
             :
         ] += 1
 
@@ -117,7 +140,7 @@ this_sample = Sample(name = Path(out_dir).name, path = out_dir)
 
 tissue_positions = pd.concat(tissue_positions_list)[['x', 'y']]
 this_sample.add_coords(
-    tissue_positions, name = "coords", mPerPx = m_per_px, size = spot_diameter_m
+    tissue_positions, name = "coords", mPerPx = m_per_px, size = SPOT_DIAMETER_M
 )
 
 this_sample.write()
