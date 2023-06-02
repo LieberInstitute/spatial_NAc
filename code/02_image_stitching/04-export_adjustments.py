9dc2c87376d0e20e3726106c385466afaca86d0e
Nick-Eagles <nick.eagles@libd.org>
2023-06-02 16:13:17 -0400

    Adjust export script to write results for any adjustments that exist (rather than doing it by explicitly naming slides); run to generate results

--- code/02_image_stitching/04-export_adjustments.py
+++ code/02_image_stitching/04-export_adjustments.py
@@ -4,41 +4,43 @@ import session_info
 import pandas as pd
 import json
 import os
-
-slides = ['V11U08-082', 'V11U23-406']
+import glob
 
 sample_info_path = here(
     'processed-data', '02_image_stitching', 'sample_info_clean.csv'
 )
 
-estimate_path = here(
-    'processed-data', '02_image_stitching', 'transformation_estimates_{}.csv'
+estimate_paths = glob.glob(
+    str(
+        here(
+            'processed-data', '02_image_stitching',
+            'transformation_estimates_*.csv'
+        )
+    )
 )
 
 out_path = here(
     'processed-data', '02_image_stitching', 'adjusted_transforms_highres.csv'
 )
 
-#   Read in sample info and subset to what we need
+#   Read in sample info and subset to relevant columns
 sample_info = pd.read_csv(sample_info_path, index_col = 0)
 sample_info = sample_info.loc[
-    sample_info['Slide #'].isin(slides),
-    ['Brain', 'Slide #', 'Array #', 'spaceranger_dir']
+    :, ['Brain', 'Slide #', 'Array #', 'spaceranger_dir']
 ]
 sample_info.index.name = 'sample_id'
 
-#   Read in and concatenate adjusted estimates for the selected slides
+#   Read in and concatenate all adjusted estimates that exist so far
 estimates = pd.concat(
-    [
-        pd.read_csv(str(estimate_path).format(slide)) for slide in slides
-    ]
+    [pd.read_csv(x, index_col = 'sample_id') for x in estimate_paths]
 )
-estimates.set_index('sample_id', inplace = True)
 
 estimates = estimates.loc[
     estimates['adjusted'], estimates.columns != 'adjusted'
 ]
 
+#   Add in some sample info (particularly we'll need the spaceranger paths to
+#   scale to high resolution)
 estimates = pd.merge(estimates, sample_info, how = 'left', on = 'sample_id')
 
 #   Scale translations to high resolution
@@ -50,6 +52,7 @@ for sample_id in estimates.index:
     with open(json_path, 'r') as f:
         spaceranger_json = json.load(f)
     
+    #   Scale to high resolution
     estimates.loc[sample_id, ['x', 'y']] *= spaceranger_json['tissue_hires_scalef']
 
 (
