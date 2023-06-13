de617ae192060ce4f5464fc7ab45e1afbf463ba1
Nick-Eagles <nick.eagles@libd.org>
2023-06-13 11:00:55 -0400

    Re-order scripts to reduce confusion

--- code/02_image_stitching/05-understand_array.sh
+++ code/02_image_stitching/05-understand_array.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #$ -cwd
 #$ -N "understand_array"
-#$ -o ../../processed-data/02_image_stitching/06-understand_array.log
-#$ -e ../../processed-data/02_image_stitching/06-understand_array.log
+#$ -o ../../processed-data/02_image_stitching/05-understand_array.log
+#$ -e ../../processed-data/02_image_stitching/05-understand_array.log
 #$ -l mf=5G,h_vmem=5G
 
 echo "**** Job starts ****"
@@ -14,7 +14,7 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 
 module load conda_R/4.3
-Rscript 06-understand_array.R
+Rscript 05-understand_array.R
 
 echo "**** Job ends ****"
 date
