66ca5d90eed60972a8a62cf39413d73b59f19826
Nick-Eagles <nick.eagles@libd.org>
2023-06-13 15:16:52 -0400

    Write a successful algorithm to map pixel locations for a merged image onto a single grid of valid array row/col values

--- code/02_image_stitching/06-spatial_coords.R
+++ code/02_image_stitching/06-spatial_coords.R
@@ -17,7 +17,7 @@ SPOT_DIAMETER_M = 55e-6
 NUM_COL = 128
 NUM_ROW = 78
 
-coords = do.call(read.csv, as.list(tissue_paths)) |> as_tibble()
+coords = do.call(rbind, lapply(tissue_paths, read.csv)) |> as_tibble()
 coords$sample_id = paste(
     ss(coords$key, '_', 2), ss(coords$key, '_', 3), sep = '_'
 )
@@ -30,25 +30,90 @@ sample_info = read.csv(sample_info_path) |>
         slide_num = 'Slide..' 
     )
 
+print("Correlation relationship between array (a) and pixel (p) row/col:")
+coords |>
+    group_by(sample_id) |>
+    summarize(
+        arow_v_prow = cor(array_row, pxl_row_in_fullres),
+        arow_v_pcol = cor(array_row, pxl_col_in_fullres),
+        acol_v_prow = cor(array_col, pxl_row_in_fullres),
+        acol_v_pcol = cor(array_col, pxl_col_in_fullres)
+    ) |>
+    print()
+
 #   Subset to 1 donor (TODO: not by slide; make more general)
 this_sample_info = sample_info |>
     filter(slide_num == 'V12D07-074')
 
 these_coords = coords |> filter(sample_id %in% this_sample_info$sample_id)
 
-#   TODO: (probably in another script) check if the relationship between
-#   array_row and pixel_row, etc is stable. Try multiple datasets and samples
+################################################################################
+#   Overwrite array_row and array_col with new values appropriate for a merged
+#   image
+################################################################################
 
-#   Horribly confusingly, 'array_row' increases with 'pxl_col_in_fullres', not
-#   'pxl_row_in_fullres'
-MAX = max(these_coords$pxl_col_in_fullres)
-MIN = min(these_coords$pxl_col_in_fullres)
-these_coords$array_row_new = round(
-    (NUM_ROW - 1) * (these_coords$pxl_col_in_fullres - MIN) / (MAX - MIN)
-)
+#   Essentially, we're placing the merged set of transformed images on a 
+#   "new Visium capture area". It contains the traditional number of rows and
+#   columns but on a much larger area. By convention, we'll have array row
+#   increase with pixel row and array col increase with pixel col.
+
+#-------------------------------------------------------------------------------
+#   Determine which array row a given spot should be assigned
+#-------------------------------------------------------------------------------
 
 MAX = max(these_coords$pxl_row_in_fullres)
 MIN = min(these_coords$pxl_row_in_fullres)
-these_coords$array_col_new = round(
-    (NUM_COL - 1) * (these_coords$pxl_row_in_fullres - MIN) / (MAX - MIN)
+these_coords$array_row = round(
+    (NUM_ROW - 1) * (these_coords$pxl_row_in_fullres - MIN) / (MAX - MIN)
 )
+
+#-------------------------------------------------------------------------------
+#   Given the newly assigned array row, determine which array column a given
+#   spot should be assigned
+#-------------------------------------------------------------------------------
+
+MAX = max(these_coords$pxl_col_in_fullres)
+MIN = min(these_coords$pxl_col_in_fullres)
+
+#   The pixel distance between array row i and row i+1
+INTERVAL_SIZE = (MAX - MIN) / (NUM_COL - 1)
+
+#   'min_vec' gives the minimum 'pxl_row_in_fullres' values possible for spots
+#   in the corresponding new array row. Odd rows have columns shifted in the
+#   positive direction by one INTERVAL_SIZE. Also, row 0 (strangely) essentially
+#   skips the first column (this is just how Visium capture areas are)
+min_vec = MIN + INTERVAL_SIZE * (these_coords$array_row %% 2 == 1)
+min_vec[match(0, these_coords$array_row)] = MIN + INTERVAL_SIZE * 2
+
+#   The interval size here is doubled because for a given row, only every other
+#   column value is possible. We multiply by 2 because we're still indexing as
+#   if every column is possible
+these_coords$array_col = 2 * round(
+        (these_coords$pxl_col_in_fullres - min_vec) / (2 * INTERVAL_SIZE)
+    ) +
+    #   Odd rows have columns start at 1, not 0
+    (these_coords$array_row %% 2 == 1)
+
+#-------------------------------------------------------------------------------
+#   Verify the newly assigned array row and cols have reasonable values
+#-------------------------------------------------------------------------------
+
+#   Even array rows can only use even column indices
+these_coords |>
+    filter(array_row %% 2 == 0) |>
+    summarize(a = all(array_col %% 2 == 0)) |>
+    pull(a) |>
+    stopifnot()
+
+#   Odd array rows can only use odd column indices
+these_coords |>
+    filter(array_row %% 2 == 1) |>
+    summarize(a = all(array_col %% 2 == 1)) |>
+    pull(a) |>
+    stopifnot()
+
+#   Check range of array row and col
+stopifnot(min(these_coords$array_row) == 0)
+stopifnot(min(these_coords$array_col) == 0)
+stopifnot(max(these_coords$array_row) == 77)
+stopifnot(max(these_coords$array_col) == 127)
