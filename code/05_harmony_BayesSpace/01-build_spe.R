1954fc329a5db8eacc249f6e5dd0b292d90f470f
Nick-Eagles <nick.eagles@libd.org>
2023-07-14 14:30:13 -0400

    Complete the SPE-creation script. Add sample_info to colData as well

--- code/05_harmony_BayesSpace/01-build_spe.R
+++ code/05_harmony_BayesSpace/01-build_spe.R
@@ -3,29 +3,41 @@ library(SpatialExperiment)
 library(here)
 library(tidyverse)
 library(jaffelab)
+library(sessioninfo)
 
 sample_info_path = here('raw-data', 'sample_key_spatial_NAc.csv')
 sample_info_path2 = here(
     'processed-data', '02_image_stitching', 'sample_info_clean.csv'
 )
 transformed_dir = here('processed-data', '04_VisiumStitcher')
+out_path = here('processed-data', '05_harmony_BayesSpace', 'spe.rds')
 
 #   Read in the two sources of sample info and merge
+message("Gathering sample info")
 sample_info = read.csv(sample_info_path) |>
     as_tibble() |>
     rename(sample_id = Slide) |>
     select(c(sample_id, Age, Sex, Diagnosis, Refined.transforms))
+
 sample_info = read.csv(sample_info_path2) |>
     as_tibble() |>
     rename(sample_id = X) |>
     right_join(sample_info, by = "sample_id") |>
     filter(Refined.transforms == "Yes") |>
-    mutate(spaceranger_dir = dirname(normalizePath(spaceranger_dir)))
+    mutate(spaceranger_dir = dirname(normalizePath(spaceranger_dir))) |>
+    rename(slide_num = Slide.., array_num = Array.., donor = Brain) |>
+    select(
+        c(
+            sample_id, donor, slide_num, array_num, spaceranger_dir,
+            raw_image_path, Age, Sex, Diagnosis
+        )
+    )
 
 ################################################################################
 #   Build the SPE object using [slide]_[capture area] as samples, to start
 ################################################################################
 
+message("Building SpatialExperiment using [slide]_[capture area] as sample ID")
 spe = read10xVisiumWrapper(
     samples = sample_info$spaceranger_dir,
     sample_id = sample_info$sample_id,
@@ -40,9 +52,11 @@ spe = read10xVisiumWrapper(
 #   Read in transformed spot coordinates and add to colData
 ################################################################################
 
+message("Adding transformed spot coordinates and sample info to colData")
+
 #   Merge all transformed spot coordinates into a single tibble
 coords_list = list()
-for (donor in unique(sample_info$Brain)) {
+for (donor in unique(sample_info$donor)) {
     #   Read in and clean up transformed spot coordinates
     coords_list[[donor]] = file.path(
             transformed_dir, donor, 'tissue_positions_list.csv'
@@ -67,10 +81,11 @@ for (donor in unique(sample_info$Brain)) {
 }
 coords = do.call(rbind, coords_list)
 
-#   Add transformed spot coordinates to colData
+#   Add transformed spot coordinates to colData. Also add sample_info
 colData(spe) = colData(spe) |>
     as_tibble() |>
     left_join(coords, by = "key") |>
+    left_join(sample_info, by = 'sample_id') |>
     DataFrame()
 
 if (any(is.na(spe$array_row_transformed))) {
@@ -81,14 +96,23 @@ if (any(is.na(spe$array_row_transformed))) {
 #   Add merged images (one image per donor)
 ################################################################################
 
-a = readImgData(
-    path = file.path(transformed_dir, unique(sample_info$Brain)),
-    sample_id = unique(sample_info$Brain),
+message("Adding merged images (one per donor) to imgData(spe)")
+
+merged_images = readImgData(
+    path = file.path(transformed_dir, unique(sample_info$donor)),
+    sample_id = unique(sample_info$donor),
     imageSources = file.path(
-        transformed_dir, unique(sample_info$Brain), "tissue_hires_image.png"
+        transformed_dir, unique(sample_info$donor), "tissue_hires_image.png"
     ),
     scaleFactors = file.path(
-        transformed_dir, unique(sample_info$Brain), "scalefactors_json.json"
+        transformed_dir, unique(sample_info$donor), "scalefactors_json.json"
     ),
     load = TRUE
 )
+
+imgData(spe) = rbind(imgData(spe), merged_images)
+
+message("Saving spe")
+saveRDS(spe, out_path)
+
+session_info()
