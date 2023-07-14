51bb1264b374098c2ef39d1c3c4ef324809bf47d
Nick-Eagles <nick.eagles@libd.org>
2023-07-14 12:35:22 -0400

    Manually read in imageData using SpatialExperiment::readImgData

--- code/05_harmony_BayesSpace/01-build_spe.R
+++ code/05_harmony_BayesSpace/01-build_spe.R
@@ -1,4 +1,5 @@
 library(spatialLIBD)
+library(SpatialExperiment)
 library(here)
 library(tidyverse)
 library(jaffelab)
@@ -7,7 +8,7 @@ sample_info_path = here('raw-data', 'sample_key_spatial_NAc.csv')
 sample_info_path2 = here(
     'processed-data', '02_image_stitching', 'sample_info_clean.csv'
 )
-transformed_dirs = here('processed-data', '04_VisiumStitcher', '{}')
+transformed_dir = here('processed-data', '04_VisiumStitcher')
 
 #   Read in the two sources of sample info and merge
 sample_info = read.csv(sample_info_path) |>
@@ -21,7 +22,10 @@ sample_info = read.csv(sample_info_path2) |>
     filter(Refined.transforms == "Yes") |>
     mutate(spaceranger_dir = dirname(normalizePath(spaceranger_dir)))
 
-#   Build the SPE object using the individual capture areas as samples, to start
+################################################################################
+#   Build the SPE object using [slide]_[capture area] as samples, to start
+################################################################################
+
 spe = read10xVisiumWrapper(
     samples = sample_info$spaceranger_dir,
     sample_id = sample_info$sample_id,
@@ -32,12 +36,17 @@ spe = read10xVisiumWrapper(
     verbose = TRUE
 )
 
+################################################################################
+#   Read in transformed spot coordinates and add to colData
+################################################################################
+
 #   Merge all transformed spot coordinates into a single tibble
 coords_list = list()
 for (donor in unique(sample_info$Brain)) {
     #   Read in and clean up transformed spot coordinates
-    coords_list[[donor]] = file.path(transformed_dirs, 'tissue_positions_list.csv') |>
-        str_replace('\\{\\}', donor) |>
+    coords_list[[donor]] = file.path(
+            transformed_dir, donor, 'tissue_positions_list.csv'
+        ) |>
         read.csv() |>
         as_tibble() |>
         mutate(
@@ -67,3 +76,19 @@ colData(spe) = colData(spe) |>
 if (any(is.na(spe$array_row_transformed))) {
     stop("Some NA transformed spot coords: number of transformed spot coords likely doesn't match ncol(spe)")
 }
+
+################################################################################
+#   Add merged images (one image per donor)
+################################################################################
+
+a = readImgData(
+    path = file.path(transformed_dir, unique(sample_info$Brain)),
+    sample_id = unique(sample_info$Brain),
+    imageSources = file.path(
+        transformed_dir, unique(sample_info$Brain), "tissue_hires_image.png"
+    ),
+    scaleFactors = file.path(
+        transformed_dir, unique(sample_info$Brain), "scalefactors_json.json"
+    ),
+    load = TRUE
+)
