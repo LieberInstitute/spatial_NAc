2b2ebd3afd15ab8808b3a411458c878e11a042f2
Nick-Eagles <nick.eagles@libd.org>
2023-08-08 16:31:47 -0400

    Update a script to use SLURM to attempt to bypass busy cluster

--- code/05_harmony_BayesSpace/01-build_spe.sh
+++ code/05_harmony_BayesSpace/01-build_spe.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #$ -cwd
 #$ -N "build_spe"
 #$ -o ../../processed-data/05_harmony_BayesSpace/logs/01-build_spe.log
@@ -6,12 +7,29 @@
 #$ -pe local 4
 #$ -l mf=10G,h_vmem=10G,h_fsize=50G
 
+#SBATCH -q shared
+#SBATCH -c 4
+#SBATCH --mem=40G
+#SBATCH --job-name=build_spe
+#SBATCH -o ../../processed-data/05_harmony_BayesSpace/logs/01-build_spe.log
+#SBATCH -e ../../processed-data/05_harmony_BayesSpace/logs/01-build_spe.log
+
+USE_SLURM=1
+
+if [[ $USE_SLURM -eq 1 ]]; then
+    job_id=$SLURM_JOB_ID
+    job_name=$SLURM_JOB_NAME
+else
+    job_id=$JOB_ID
+    job_name=$JOB_NAME
+fi
+
 echo "**** Job starts ****"
 date
 echo "**** JHPCE info ****"
 echo "User: ${USER}"
-echo "Job id: ${JOB_ID}"
-echo "Job name: ${JOB_NAME}"
+echo "Job id: ${job_id}"
+echo "Job name: ${job_name}"
 echo "Hostname: ${HOSTNAME}"
 
 module load conda_R/4.3
