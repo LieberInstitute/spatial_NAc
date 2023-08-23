edb2bd979d2e1ecf633b4cb198546a09a686df39
Nick-Eagles <nick.eagles@libd.org>
2023-08-23 14:32:03 -0400

    Only use alphanumeric characters in image channel names as required by Samui

--- code/02_image_stitching/01-samui_test.py
+++ code/02_image_stitching/01-samui_test.py
@@ -460,7 +460,7 @@ this_sample.add_coords(
 
 this_sample.add_image(
     tiff = img_out_browser_path,
-    channels = sample_info.index.tolist(),
+    channels = [x.replace('-', 'x') for x in sample_info.index],
     scale = m_per_px
 )
 
