1cbc51596d0c86fef6eb1baa2190bbc2851f7af7
Nick-Eagles <nick.eagles@libd.org>
2023-09-01 16:10:54 -0400

    Loop through every sample to get an initial idea of how merged donors look

--- code/05_harmony_BayesSpace/test_plotting.R
+++ code/05_harmony_BayesSpace/test_plotting.R
@@ -6,26 +6,25 @@ library(jaffelab)
 library(sessioninfo)
 source(here('code', '05_harmony_BayesSpace', 'plotting_functions.R'))
 
-spe_path = here('processed-data', '05_harmony_BayesSpace', 'spe.rds')
-plot_dir = here('plots', '05_harmony_BayesSpace')
+spe_path = here('processed-data', '05_harmony_BayesSpace', 'spe_filtered.rds')
+plot_dir = here('plots', '05_harmony_BayesSpace', 'test_plots')
 
 sample_id = "Br2720"
-discrete_var = "X10x_kmeans_7_clusters"
+discrete_var = "10x_kmeans_7_clusters"
 cont_var = "sum_gene"
+
+dir.create(plot_dir, showWarnings = FALSE)
 
 spe = readRDS(spe_path)
 
-# colnames(spe) = spe$key
-# spe$pxl_row_in_fullres_original <- spe$pxl_row_in_fullres
-# spe$pxl_col_in_fullres_original <- spe$pxl_col_in_fullres
-# spe$pxl_row_in_fullres <- spe$pxl_col_in_fullres <- NULL
+for (donor in unique(spe$sample_id)[unique(spe$sample_id) != "Br6432"]) {
+    p = vis_merged(spe, sampleid = donor, coldatavar = discrete_var)
+    pdf(file.path(plot_dir, sprintf('vis_merged_discrete_%s.pdf', donor)))
+    print(p)
+    dev.off()
 
-p = vis_merged(spe, sampleid = sample_id, coldatavar = discrete_var)
-pdf(file.path(plot_dir, 'vis_merged_discrete.pdf'))
-print(p)
-dev.off()
-
-p = vis_merged(spe, sampleid = sample_id, coldatavar = cont_var)
-pdf(file.path(plot_dir, 'vis_merged_continuous.pdf'))
-print(p)
-dev.off()
+    p = vis_merged(spe, sampleid = donor, coldatavar = cont_var)
+    pdf(file.path(plot_dir, sprintf('vis_merged_continuous_%s.pdf', donor)))
+    print(p)
+    dev.off()
+}
