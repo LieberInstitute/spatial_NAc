69bbdb7b96874fc6bc04612ae576ded1dde2a3de
Ryan Miller <ry.a.miller3@gmail.com>
2023-09-13 16:22:07 -0400

    vistoseg splitslide function, loupe alignment and preperation for spaceranger outputs for samples transferred on 230911

---
+++ raw-data/FASTQ_reorg/raw-mkdir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --array=1-6
+
+SAMPLE=$(awk "NR==${SLURM_ARRAY_TASK_ID}" raw-mkdir.txt)
+
+mkdir -p ./logs/
+mkdir -p /dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/raw-data/FASTQ_reorg/${SAMPLE}/
+
+mv slurm-*.out ./logs
