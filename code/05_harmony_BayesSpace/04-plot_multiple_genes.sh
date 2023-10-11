907b95081312a7a5dcc1d1082d2c135ba81972ef
Nick-Eagles <nick.eagles@libd.org>
2023-10-11 14:08:21 -0400

    Fix typo

--- code/05_harmony_BayesSpace/04-plot_multiple_genes.sh
+++ code/05_harmony_BayesSpace/04-plot_multiple_genes.sh
@@ -15,7 +15,7 @@ echo "**** JHPCE info ****"
 echo "User: ${USER}"
 echo "Job id: ${SLURM_JOB_ID}"
 echo "Job name: ${SLURM_JOB_NAME}"
-echo "Node name: ${$SLURMD_NODENAME}"
+echo "Node name: ${SLURMD_NODENAME}"
 
 module load conda_R/4.3
 Rscript 04-plot_multiple_genes.R
