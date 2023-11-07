c63fe8f792c50de1cc5eb0cdb5aafc6701d43fc1
Nick-Eagles <nick.eagles@libd.org>
2023-11-07 13:09:41 -0500

    Fix how PCA is computed: compute HVGs and subset as conventional

--- code/05_harmony_BayesSpace/02-filter_normalize_spe.R
+++ code/05_harmony_BayesSpace/02-filter_normalize_spe.R
@@ -7,13 +7,15 @@ library(sessioninfo)
 library(scran)
 library(BiocParallel)
 library(spatialNAcUtils)
-library(glmpca)
+library(scran)
+library(scater)
 
 sample_info_path = here('raw-data', 'sample_key_spatial_NAc.csv')
 sample_info_path2 = here(
     'processed-data', '02_image_stitching', 'sample_info_clean.csv'
 )
 transformed_dir = here('processed-data', '04_VisiumStitcher')
+processed_dir = here("processed-data", "05_harmony_BayesSpace")
 raw_in_path = here('processed-data', '05_harmony_BayesSpace', 'spe_raw.rds')
 filtered_out_path = here(
     'processed-data', '05_harmony_BayesSpace', 'spe_filtered.rds'
@@ -27,7 +29,13 @@ num_red_dims = 20
 ################################################################################
 
 spe = readRDS(raw_in_path)
-spe <- spe[,spe$in_tissue]
+
+#   Filter SPE: take only spots in tissue, drop spots with 0 counts for all
+#   genes, and drop genes with 0 counts in every spot
+spe <- spe[
+    rowSums(assays(spe)$counts) > 0,
+    (colSums(assays(spe)$counts) > 0) & spe$in_tissue
+]
 
 #   Compute outlier spots by library size
 spe$scran_low_lib_size <-
@@ -92,8 +100,58 @@ message("Running logNormCounts()")
 spe <- logNormCounts(spe)
 
 ################################################################################
-#   Compute PCA and GLM PCA
+#   Compute PCA
 ################################################################################
+
+message("Running modelGeneVar()")
+## From
+## http://bioconductor.org/packages/release/bioc/vignettes/scran/inst/doc/scran.html#4_variance_modelling
+dec <- modelGeneVar(
+    spe,
+    block = spe$sample_id_original,
+    BPPARAM = MulticoreParam(num_cores)
+)
+
+pdf(file.path(plot_dir, "scran_modelGeneVar.pdf"), useDingbats = FALSE)
+mapply(function(block, blockname) {
+    plot(
+        block$mean,
+        block$total,
+        xlab = "Mean log-expression",
+        ylab = "Variance",
+        main = blockname
+    )
+    curve(metadata(block)$trend(x),
+        col = "blue",
+        add = TRUE
+    )
+}, dec$per.block, names(dec$per.block))
+dev.off()
+
+message("Running getTopHVGs()")
+top.hvgs <- getTopHVGs(dec, prop = 0.1)
+
+top.hvgs.fdr5 <- getTopHVGs(dec, fdr.threshold = 0.05)
+print(paste("Num HVGs at FDR = 0.05:", length(top.hvgs.fdr5)))
+
+top.hvgs.fdr1 <- getTopHVGs(dec, fdr.threshold = 0.01)
+print(paste("Num HVGs at FDR = 0.01:", length(top.hvgs.fdr1)))
+
+save(
+    top.hvgs, top.hvgs.fdr5, top.hvgs.fdr1,
+    file = file.path(processed_dir, "top.hvgs.Rdata")
+)
+
+message("Running runPCA()")
+Sys.time()
+spe <- runPCA(spe, subset_row = top.hvgs, ncomponents = num_red_dims)
+Sys.time()
+
+#   Plot variance explained
+percent.var <- attr(reducedDim(spe, "PCA"), "percentVar")
+pdf(file.path(dir_plots, "pca_elbow.pdf"), useDingbats = FALSE)
+plot(percent.var, xlab = "PC", ylab = "Variance explained (%)")
+dev.off()
 
 message("Adding PCA to 'reducedDims(spe)'")
 pca = prcomp(t(assays(spe)$counts), rank = num_red_dims)
