c52bd5dc13a758ced0b563db783439db1961a2e0
Nick-Eagles <nick.eagles@libd.org>
2023-11-13 15:00:33 -0500

    Add more code used in ultimately testing spatialNAcUtils::spe_to_seurat

--- code/09_seurat_spe_conversion/01-convert.R
+++ code/09_seurat_spe_conversion/01-convert.R
@@ -8,16 +8,58 @@ library(SeuratData)
 spe_in = here(
     'processed-data', '05_harmony_BayesSpace', 'spe_filtered.rds'
 )
-spot_diameter = 55e-6
+
 
 #   Given a SpatialExperiment 'spe', convert to a Seurat object and return
-spe_to_seurat = function(spe) {
+spe_to_seurat = function(spe, spot_diameter = 55e-6, verbose = TRUE) {
+    if (verbose) message("Running 'as.Seurat(spe)'...")
     seur = as.Seurat(spe)
 
+    for (sample_id in unique(spe$sample_id)) {
+        if (verbose) {
+            message(
+                sprintf(
+                    "Adding spot coordinates and images for sample %s...",
+                    sample_id
+                )
+            )
+        }
+        spe_small = spe[, spe$sample_id == sample_id]
+
+        coords = colData(spe_small)[
+            ,
+            c(
+                'in_tissue', 'array_row_transformed', 'array_col_transformed',
+                'pxl_row_in_fullres_transformed', 'pxl_col_in_fullres_transformed'
+            )
+        ]
+        colnames(coords) = c('tissue', 'row', 'col', 'imagerow', 'imagecol')
+        coords$tissue = as.integer(coords$tissue)
+        coords = as.data.frame(coords)
+
+        this_img = array(
+            t(col2rgb(imgRaster(spe_small))),
+            dim = c(dim(imgRaster(spe_small)), 3)
+        ) / 256
+
+        seur@images[[sample_id]] = Seurat:::VisiumV1(
+            image = this_img,
+            scale.factors = scalefactors(
+                spot = NA, fiducial = NA, hires = NA, lowres = scaleFactors(spe_small)
+            ),
+            coordinates = coords,
+            spot.radius = spot_diameter / scaleFactors(spe_small),
+            assay = "originalexp",
+            key = paste0(sample_id, '_')
+        )
+    }
+
+    if (verbose) message("Returning converted object...")
     return(seur)
 }
 
 spe = readRDS(spe_in)
+spe = spe[, !is.na(spe$exclude_overlapping)]
 seur_ex = LoadData("stxBrain", type = "anterior1") # InstallData("stxBrain")
 
 this_sample = unique(spe$donor)[2]
@@ -45,6 +87,9 @@ a = t(col2rgb(t(imgRaster(spe_small)))) # makes little repeated shapes?
 a = col2rgb(imgRaster(spe_small)) # vertical stripes with pink on left and blue on right
 a = col2rgb(t(imgRaster(spe_small))) # RGB diagonal stripes
 
+a = col2rgb(t(imgRaster(spe_small)))
+a = rep(a[1,], times = 3)
+
 this_img = array(
     a,
     dim = c(dim(imgRaster(spe_small)), 3)
@@ -53,14 +98,17 @@ this_img = array(
 seur_spe@images[[this_sample]] = Seurat:::VisiumV1(
     image = this_img,
     scale.factors = scalefactors(
-        spot = NA, fiducial = NA, hires = NA, lowres = scaleFactors(spe_small) / 3
+        spot = NA, fiducial = 100, hires = NA, lowres = scaleFactors(spe_small)
     ),
     coordinates = coords,
     spot.radius = spot_diameter / scaleFactors(spe_small),
-    assay = "Spatial",
+    assay = "originalexp",
     key = paste0(this_sample, '_')
 )
 names(seur_spe@images) = this_sample # why is this necessary?
 
 # SpatialFeaturePlot(seur_ex, features = c("Hpca", "Ttr"))
 SpatialFeaturePlot(seur_spe, features = rownames(seur_spe)[1:2])#, images = this_sample)
+
+FindSpatiallyVariableFeatures(seur_spe, assay = "originalexp", features = VariableFeatures(seur_spe)[1:1000],
+    selection.method = "moransi")
