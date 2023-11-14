db3a2bab4ab107d7783df80493f801628c100dd4
Nick-Eagles <nick.eagles@libd.org>
2023-11-14 11:57:23 -0500

    Add an initial script to run nnSVG for one sample. Will be part of an array job across all samples

---
+++ code/05_harmony_BayesSpace/05-run_nnSVG.R
@@ -0,0 +1,65 @@
+library(spatialLIBD)
+library(SpatialExperiment)
+library(here)
+library(tidyverse)
+library(jaffelab)
+library(sessioninfo)
+library(spatialNAcUtils)
+library(HDF5Array)
+library(nnSVG)
+
+spe_dir = here(
+    'processed-data', '05_harmony_BayesSpace', 'spe_filtered_hdf5'
+)
+
+sample_id = unique(spe$sample_id)[as.integer(Sys.getenv('SLURM_ARRAY_TASK_ID'))]
+out_path = here(
+    'processed-data', '05_harmony_BayesSpace', 'nnSVG_out', paste0(sample_id, '.csv')
+)
+
+set.seed(0)
+dir.create(dirname(out_path), showWarnings = FALSE)
+
+message(Sys.time(), ' | Loading SpatialExperiment')
+spe = loadHDF5SummarizedExperiment(spe_dir)
+
+#-------------------------------------------------------------------------------
+#   Subset to this sample, filter lowly expressed and mitochondrial genes, and
+#   take spots with at least some nonzero counts
+#-------------------------------------------------------------------------------
+
+message(Sys.time(), ' | Filtering genes, and spots, and to ', sample_id)
+spe = spe[, spe$sample_id == sample_id]
+
+#   Bring fully into memory to speed up computations later
+assays(spe) = list(counts = assays(spe)$counts)
+spe = realize(spe)
+
+spe = filter_genes(
+    spe, 
+    filter_genes_ncounts = 3, 
+    filter_genes_pcspots = 0.5, 
+    filter_mito = TRUE
+)
+spe = spe[, colSums(assays(spe)$counts) > 0]
+
+#-------------------------------------------------------------------------------
+#   Recompute logcounts (library-size normalization as recommended in
+#   https://bioconductor.org/packages/release/bioc/vignettes/nnSVG/inst/doc/nnSVG.html)
+#-------------------------------------------------------------------------------
+
+message(Sys.time(), ' | Re-computing logcounts')
+spe = computeLibraryFactors(spe)
+spe = logNormCounts(spe)
+
+#-------------------------------------------------------------------------------
+#   Run nnSVG and export results
+#-------------------------------------------------------------------------------
+
+message(Sys.time(), ' | Running nnSVG')
+spe = nnSVG(spe)
+
+message(Sys.time(), ' | Exporting results')
+write.csv(rowData(spe), out_path, row.names = FALSE, quote = FALSE)
+
+session_info()
