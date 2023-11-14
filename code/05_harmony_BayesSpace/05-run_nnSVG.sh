a47fcca675a1daabe01e3fd4633ce8578c85c24e
Nick-Eagles <nick.eagles@libd.org>
2023-11-14 16:56:55 -0500

    Add a shell script to run nnSVG as an array job

---
+++ code/05_harmony_BayesSpace/05-run_nnSVG.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH -c 1
+#SBATCH --mem=30G
+#SBATCH --job-name=05-run_nnSVG
+#SBATCH -o ../../processed-data/05_harmony_BayesSpace/logs/05-run_nnSVG_%a.log
+#SBATCH -e ../../processed-data/05_harmony_BayesSpace/logs/05-run_nnSVG_%a.log
+#SBATCH --array=1-2%2
+
+set -e
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+
+module load conda_R/4.3
+Rscript 05-run_nnSVG.R
+
+echo "**** Job ends ****"
+date
