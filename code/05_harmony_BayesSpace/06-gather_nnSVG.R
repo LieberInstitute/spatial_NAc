37eff7d4609e1c01bd06c34052a5b648af955d49
Nick-Eagles <nick.eagles@libd.org>
2023-12-01 16:13:43 -0500

    Add code to explore nnSVG results and improve script organization

--- code/05_harmony_BayesSpace/06-gather_nnSVG.R
+++ code/05_harmony_BayesSpace/06-gather_nnSVG.R
@@ -11,12 +11,19 @@ spe_dir = here(
     'processed-data', '05_harmony_BayesSpace', 'spe_filtered_hdf5'
 )
 nn_out_dir = here('processed-data', '05_harmony_BayesSpace', 'nnSVG_out')
+hvg_path = here("processed-data", "05_harmony_BayesSpace", "top.hvgs.Rdata")
+plot_dir = here('plots', '05_harmony_BayesSpace')
 sig_cutoff = 0.05
+best_sample_id = 'Br8492'
 
 spe = loadHDF5SummarizedExperiment(spe_dir)
 
+################################################################################
+#   Gather nnSVG results across samples and compute summary metrics
+################################################################################
+
 nn_out_list = list()
-for (sample_id in unique(spe$sample_id)[2:10]) {
+for (sample_id in unique(spe$sample_id)) {
     nn_out_list[[sample_id]] = file.path(nn_out_dir, paste0(sample_id, '.csv')) |>
         read.csv() |>
         as_tibble()
@@ -35,6 +42,35 @@ nn_out_summary = nn_out |>
     #   Compute a rank of average ranks
     mutate(nnsvg_avg_rank_rank = match(nnsvg_avg_rank, sort(nnsvg_avg_rank)))
 
+################################################################################
+#   Visually examine top-ranked SVGs
+################################################################################
+
+top_genes = nn_out_summary |>
+    filter(nnsvg_avg_rank_rank <= 5) |>
+    pull(gene_id)
+
+plot_list = lapply(
+    top_genes,
+    function(gene) {
+        spot_plot(
+            spe,
+            sample_id = best_sample_id,
+            var_name = gene,
+            is_discrete = FALSE,
+            minCount = 0
+        )
+    }
+)
+
+pdf(file.path(plot_dir, 'nnSVG_top_SVGs.pdf'))
+print(plot_list)
+dev.off()
+
+################################################################################
+#   Update SpatialExperiment with nnSVG results
+################################################################################
+
 #   Add summary metrics from nnSVG to the SpatialExperiment
 rowData(spe) = rowData(spe) |>
     as_tibble() |>
@@ -43,4 +79,8 @@ rowData(spe) = rowData(spe) |>
     DataFrame()
 quickResaveHDF5SummarizedExperiment(spe)
 
+nn_out_summary |>
+    filter(nnsvg_avg_rank_rank <= 0.1 * nrow(spe))
+
+load(hvg_path)
 session_info()
