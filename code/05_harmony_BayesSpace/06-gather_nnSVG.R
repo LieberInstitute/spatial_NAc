b7f258a9c6291b54800d66d966ab8178393a69e7
Nick-Eagles <nick.eagles@libd.org>
2023-12-12 15:08:40 -0500

    Also plot a 5x5 grid of 5 samples and 5 top-ranked genes

--- code/05_harmony_BayesSpace/06-gather_nnSVG.R
+++ code/05_harmony_BayesSpace/06-gather_nnSVG.R
@@ -6,6 +6,7 @@ library(sessioninfo)
 library(spatialNAcUtils)
 library(HDF5Array)
 library(nnSVG)
+library(cowplot)
 
 spe_dir = here(
     'processed-data', '05_harmony_BayesSpace', 'spe_filtered_hdf5'
@@ -48,17 +49,17 @@ nn_out_summary = nn_out |>
 #   Visually examine top-ranked SVGs
 ################################################################################
 
-#   Take top-ranked genes of those where all samples had statistically
+#   Order rank of genes of those where all samples had statistically
 #   significant spatial variability
-num_genes = 10
 top_genes = nn_out_summary |>
     filter(nnsvg_prop_sig_adj == 1) |>
     arrange(nnsvg_avg_rank_rank) |>
-    slice_head(n = num_genes) |>
     mutate(
         symbol = rowData(spe)[match(gene_id, rowData(spe)$gene_id), 'gene_name']
     )
 
+#   Plot the top 10 genes for one sample
+num_genes = 10
 plot_list = list()
 for (i in 1:num_genes) {
     plot_list[[i]] = spot_plot(
@@ -71,8 +72,36 @@ for (i in 1:num_genes) {
     )
 }
 
-pdf(file.path(plot_dir, 'nnSVG_top_SVGs.pdf'))
+pdf(file.path(plot_dir, sprintf('nnSVG_top_SVGs_%s.pdf', best_sample_id)))
 print(plot_list)
+dev.off()
+
+#   Plot a grid of several samples and several top-ranked genes
+num_genes = 5
+num_samples = 5
+plot_list = list()
+counter = 1
+for (i in 1:num_genes) {
+    for (j in 1:num_samples) {
+        this_sample_id = unique(spe$sample_id)[j]
+        plot_list[[counter]] = spot_plot(
+            spe,
+            sample_id = this_sample_id,
+            title = paste(this_sample_id, top_genes$symbol[i], sep = '_'),
+            var_name = top_genes$gene_id[i],
+            is_discrete = FALSE,
+            minCount = 0
+        )
+        counter = counter + 1
+    }
+}
+
+pdf(
+    file.path(plot_dir, 'nnSVG_grid.pdf'),
+    width = 7 * num_samples,
+    height = 7 * num_genes
+)
+print(plot_grid(plotlist = plot_list, ncol = num_samples))
 dev.off()
 
 ################################################################################
