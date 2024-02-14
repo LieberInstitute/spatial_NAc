800efa88f7a362ad6d53929e6136be39fcc348ec
Nick-Eagles <nick.eagles@libd.org>
2024-02-14 11:32:55 -0500

    Borrow modeling code from https://github.com/LieberInstitute/spatialDLPFC/blob/14a1f253a92e43c01fec3cc3077a9b2cf9ce9fc0/code/analysis/07_layer_differential_expression/03_model_BayesSpace.R#L61-L101
    Co-authored-by: Leonardo Collado Torres <lcolladotor@gmail.com>

--- code/10_precast/04-model_precast.R
+++ code/10_precast/04-model_precast.R
@@ -26,7 +26,47 @@ modeling_path = here(
 
 spe_pseudo = readRDS(spe_pseudo_path)
 
-#   Modeling code
+registration_mod <-
+    registration_model(sce_pseudo, covars = covars)
+
+block_cor <-
+    registration_block_cor(sce_pseudo, registration_model = registration_mod)
+
+results_enrichment <-
+    registration_stats_enrichment(
+        sce_pseudo,
+        block_cor = block_cor,
+        covars = covars,
+        gene_ensembl = gene_ensembl,
+        gene_name = gene_name
+    )
+results_pairwise <-
+    registration_stats_pairwise(
+        sce_pseudo,
+        registration_model = registration_mod,
+        block_cor = block_cor,
+        gene_ensembl = gene_ensembl,
+        gene_name = gene_name
+    )
+if (k >= 3) {
+    results_anova <-
+        registration_stats_anova(
+            sce_pseudo,
+            block_cor = block_cor,
+            covars = covars,
+            gene_ensembl = gene_ensembl,
+            gene_name = gene_name,
+            suffix = suffix
+        )
+} else {
+    results_anova <- NULL
+}
+
+modeling_results <- list(
+    "anova" = results_anova,
+    "enrichment" = results_enrichment,
+    "pairwise" = results_pairwise
+)
 
 save(modeling_results, modeling_path)
 
