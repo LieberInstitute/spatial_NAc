c95b0913437459426c2fd98158e24735a11decc9
Rphillips1995 <Rphillips11995@gmail.com>
2024-02-27 08:46:12 -0500

    GLMPCA + MNN batch correction

--- code/12_build_sce/05_DimensionalityReduction.R
+++ code/12_build_sce/05_DimensionalityReduction.R
@@ -10,11 +10,12 @@ library(scran)
 library(scry)
 library(here)
 
+
 #Load sce 
-sce <- loadHDF5SummarizedExperiment(dir = here("processed-data","12_snRNA",
-                                               "sce_featureselection"))
+load(here("processed-data","12_snRNA","sce_featureselection.rds"),verbose = TRUE)
 
 sce
+
 
 #Take top 2000 highly deviant genes
 hdgs <- rownames(sce)[order(rowData(sce)$binomial_deviance, decreasing = T)][1:2000]
@@ -23,9 +24,10 @@ hdgs.symbols <- rowData(sce)$gene_name[match(hdgs, rowData(sce)$gene_id)]
 #Checkl if Dopamine receptors and PPP1R1B are among the highly deviant genes
 c("DRD1","DRD2","PPP1R1B") %in% hdgs.symbols
 
+print("Running PCA")
 #Run PCA
 sce <- runPCA(sce,
-              exprs_values = "binomial_deviance_residuals",
+              exprs_values = "binomial_pearson_residuals",
               subset_row = hdgs, 
               ncomponents = 100,
               name = "GLMPCA_approx")
@@ -37,15 +39,16 @@ PCA_plots <- plotReducedDim(sce,
                             ncomponents = 6, 
                             point_alpha = 0.3)
 ggsave(PCA_plots,filename = here("plots","12_snRNA","Dim_Red","Top6_PCAs.png"))
+print("PCA Complete")
 
-
-#t-SNE with top 50 dimensions. 
+#t-SNE with top 50 dimensions.
+print("Running tSNE") 
 set.seed(100111001)
 sce <- runTSNE(sce,
                dimred = "GLMPCA_approx",
                n_dimred = 50,
                name = "tSNE")
-
+print("tSNE complete")
 #Visualize t-SNE with points colored by sample
 tsne_sample <- plotReducedDim(sce,
                               dimred = "tSNE", 
@@ -55,9 +58,10 @@ ggsave(tsne_sample,filename = here("plots","12_snRNA","Dim_Red","tSNE_Sample_noM
 #Looks like some sections af the tSNE are dominated by just a few of the samples. 
 
 #Plot by snRNA_date
+#Misspelled as snRNA_data, will fix later. 
 tsne_snRNA_date <- plotReducedDim(sce,
                                   dimred = "tSNE", 
-                                  colour_by = "snRNA_date",
+                                  colour_by = "snRNA_data",
                                   point_alpha = 0.3)
 ggsave(tsne_snRNA_date,filename = here("plots","12_snRNA","Dim_Red","tSNE_snRNA_date_noMNN.png"))
 
@@ -70,13 +74,16 @@ ggsave(tsne_sort,filename = here("plots","12_snRNA","Dim_Red","tSNE_SortType_noM
 
 #Batch correction most likely needed here. 
 #Run batch correction with mutual nearest neighbors. 
+print("Running MNN")
 glmpca_mnn <- batchelor::reducedMNN(reducedDim(sce, "GLMPCA_approx"),
                                     batch=as.factor(sce$Sample))
+print("MNN complete")
 
 #Add mnn to the object
 reducedDim(sce,"mnn") <- glmpca_mnn$corrected
 
 #tSNE post-mnn with top 50 dimensions
+print("Running tSNE post-MNN")
 set.seed(100111001)
 sce <- runTSNE(sce,
                dimred = "mnn",
@@ -90,12 +97,17 @@ tSNE_mnn_Sample <- plotReducedDim(sce,
 ggsave(tSNE_mnn_Sample,
        filename = here("plots","12_snRNA","Dim_Red","tSNE_Sample_mnn.png"))
 
+
+print("tSNE_mnn complete")
+
 #Save object with Dimensionality Reduction
+#Switching to save with HDF5 here because at this point there is so much information within the sce
+print("Saving sce object")
 saveHDF5SummarizedExperiment(sce,
                              here("processed-data","12_snRNA",
                                   "sce_DimRed"),
                              replace = TRUE)
-
+print("Object saved")
 print("Reproducibility information:")
 Sys.time()
 proc.time()
--- code/12_build_sce/05_DimensionalityReduction.sh
+++ code/12_build_sce/05_DimensionalityReduction.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 #
-#SBATCH --job-name=DimRed
-#SBATCH --output=DimRed.out
-#SBATCH --error=DimRed.err
+#SBATCH --job-name=05_DimRed
+#SBATCH --output=logs/05_DimensionalityReduction.log
+#SBATCH --error=logs/05_DimensionalityReduction.log
 #
 # Number of CPUs allocated to each task.
 #SBATCH --cpus-per-task=2
 #
 # Mimimum memory required per allocated  CPU
-#SBATCH --mem-per-cpu=40G
+#SBATCH --mem-per-cpu=80G
 #
 # Send mail to the email address when the job fails
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=robert.phillips@libd.org
-
-cd /dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc
 
 echo "********* Job Starts *********"
 date
@@ -22,11 +20,8 @@ date
 #load R
 module load r_nac
 
-#list modules for reproducibility purposes
-module list
-
 #run the Rjob
-R CMD BATCH /dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/code/12_build_sce/05_DimensionalityReduction.R
+Rscript 05_DimensionalityReduction.R
 
 echo "********* Job Ends *********"
 date
