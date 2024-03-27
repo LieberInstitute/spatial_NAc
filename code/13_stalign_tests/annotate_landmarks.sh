029fa29b5f3a89047b50d9bab3a631f9a7b98086
Nick-Eagles <nick.eagles@libd.org>
2024-03-27 11:40:06 -0400

    Add a script to manually annotate landmarks between 2 capture areas

---
+++ code/13_stalign_tests/annotate_landmarks.sh
@@ -0,0 +1,4 @@
+module load stalign/1.0.1
+python $(which point_annotator.py) \
+    ../../processed-data/13_stalign_tests/src_image.npz \
+    ../../processed-data/13_stalign_tests/target_image.npz
