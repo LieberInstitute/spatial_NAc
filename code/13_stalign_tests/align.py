596e369fb63cc31f48863d6876fbd2fb2a2eedf1
Nick-Eagles <nick.eagles@libd.org>
2024-03-29 08:50:25 -0400

    Read in manual annotation and plot overtop capture areas

--- code/13_stalign_tests/align.py
+++ code/13_stalign_tests/align.py
@@ -10,13 +10,19 @@ from pathlib import Path
 from pyhere import here
 from PIL import Image
 
+#   Set a non-interactive backend for writing figures to a file
+import matplotlib
+matplotlib.use('agg')
+
 sample_info_path = here(
     'processed-data', '02_image_stitching', 'sample_info_clean.csv'
 )
 out_dir = Path(here('processed-data', '13_stalign_tests'))
+plot_dir = Path(here('plots', '13_stalign_tests'))
 capture_areas = ['V12D07-078_B1', 'V12D07-078_D1']
 
 out_dir.mkdir(parents = False, exist_ok = True)
+plot_dir.mkdir(parents = False, exist_ok = True)
 
 ################################################################################
 #   Read in source and target images and normalize
@@ -68,3 +74,35 @@ for i in pointsJlist.keys():
 
 pointsI = np.array(pointsI)
 pointsJ = np.array(pointsJ)
+
+################################################################################
+#   Plot capture areas with landmarks overlayed
+################################################################################
+
+fig,ax = plt.subplots(1,2)
+ax[0].imshow((I.transpose(1,2,0).squeeze()), extent=extentI)
+ax[1].imshow((J.transpose(1,2,0).squeeze()), extent=extentJ)
+
+trans_offset_0 = mtransforms.offset_copy(
+    ax[0].transData, fig=fig, x=0.05, y=-0.05, units='inches'
+)
+trans_offset_1 = mtransforms.offset_copy(
+    ax[1].transData, fig=fig, x=0.05, y=-0.05, units='inches'
+)
+
+ax[0].scatter(pointsI[:,1],pointsI[:,0], c='red', s=10)
+ax[1].scatter(pointsJ[:,1],pointsJ[:,0], c='red', s=10)
+
+for i in pointsIlist.keys():
+    for j in range(len(pointsIlist[i])):
+        ax[0].text(pointsIlist[i][j][0], pointsIlist[i][j][1],f'{i}{j}', c='red', transform=trans_offset_0, fontsize= 8)
+
+for i in pointsJlist.keys():
+    for j in range(len(pointsJlist[i])):
+        ax[1].text(pointsJlist[i][j][0], pointsJlist[i][j][1],f'{i}{j}', c='red', transform=trans_offset_1, fontsize= 8)
+
+ax[0].set_title(capture_areas[0], fontsize=15)
+ax[1].set_title(capture_areas[1], fontsize=15)
+
+plt.savefig(str(plot_dir / '_'.join(capture_areas)) + '_with_landmarks.png')
+plt.clf()
