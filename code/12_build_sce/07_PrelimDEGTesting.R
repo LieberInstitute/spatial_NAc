28fe73605d090858d6317134358734ce76a8c3cb
Rphillips1995 <Rphillips11995@gmail.com>
2024-03-29 14:54:59 -0400

    prelim DEG testing at .25 resolution

---
+++ code/12_build_sce/07_PrelimDEGTesting.R
@@ -0,0 +1,56 @@
+#cd /dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/
+#module load r_nac
+
+library(SingleCellExperiment)
+library(DeconvoBuddies)
+library(sessioninfo)
+library(HDF5Array)
+library(scran)
+library(here)
+
+##############
+#Load sce
+print("Loading SCE")
+sce <- loadHDF5SummarizedExperiment(dir = here("processed-data","12_snRNA",
+                                               "sce_clustered"))
+
+dim(sce)
+
+stopifnot(identical(rownames(colData(sce)),colnames(sce)))
+
+sce
+###############
+print("Starting DEG testing")
+mod <- with(colData(sce), model.matrix(~ Sample))
+mod <- mod[ , -1, drop=F] # intercept otherwise automatically dropped by `findMarkers()`
+
+# Run pairwise t-tests
+markers_pairwise <- findMarkers(sce, 
+                                groups=sce$k_10_louvain_pt25,
+                                assay.type="logcounts", 
+                                design=mod, 
+                                test="t",
+                                direction="up", 
+                                pval.type="all", 
+                                full.stats=T)
+
+#How many DEGs for each cluster? 
+sapply(markers_pairwise, function(x){table(x$FDR<0.05)})
+
+#Add gene info to each list.
+for(i in names(markers_pairwise)){
+    markers_pairwise[[i]] <- as.data.frame(markers_pairwise[[i]])
+    markers_pairwise[[i]]$gene_id <- row.names(markers_pairwise[[i]])
+    markers_pairwise[[i]] <- dplyr::left_join(x  =  markers_pairwise[[i]],
+                                              y  =  as.data.frame(rowData(sce)[,c("gene_id","gene_name")]),
+                                              by = "gene_id")
+}
+
+
+save(markers_pairwise,file = here("processed-data","12_snRNA","markers_pairwise_list_k_10_louvain_pt25.rda"))
+
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
