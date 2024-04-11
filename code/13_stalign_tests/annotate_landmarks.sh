9e58d363e1af2687b7a33e5eef77bd0457912fd3
Nick-Eagles <nick.eagles@libd.org>
2024-04-11 11:12:41 -0400

    Support annotating different image resolutions

--- code/13_stalign_tests/annotate_landmarks.sh
+++ code/13_stalign_tests/annotate_landmarks.sh
@@ -1,4 +1,6 @@
+resolution=lowres
+
 module load stalign/1.0.1
 python $(which point_annotator.py) \
-    ../../processed-data/13_stalign_tests/src_image.npz \
-    ../../processed-data/13_stalign_tests/target_image.npz
+    ../../processed-data/13_stalign_tests/src_image_${resolution}.npz \
+    ../../processed-data/13_stalign_tests/target_image_${resolution}.npz
