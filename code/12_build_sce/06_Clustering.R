f74fc3067c4a2129cc89982ed37ba84680c43766
Rphillips1995 <Rphillips11995@gmail.com>
2024-05-13 11:20:34 -0400

    k=20 + walktrap clustering. Calculate logcounts with walktrap clusters

--- code/12_build_sce/06_Clustering.R
+++ code/12_build_sce/06_Clustering.R
@@ -4,7 +4,6 @@
 #Load libraries
 library(SingleCellExperiment)
 library(sessioninfo)
-library(HDF5Array)
 library(batchelor)
 library(rafalib)
 library(ggplot2)
@@ -14,11 +13,13 @@ library(scran)
 library(here)
 
 #load sce
-sce <- loadHDF5SummarizedExperiment(dir = here("processed-data",
-                                               "12_snRNA",
-                                               "sce_DimRed"))
 
+print("Loading Object")
+Sys.time()
+sce <- readRDS(here("processed-data","12_snRNA","sce_DimRed.Rds"))
 print("Object loaded")
+Sys.time()
+
 sce
 
 #Additional sanity chekc
@@ -29,120 +30,46 @@ sce
 
 #######################################
 #Begin clustering workflow.
-print("Clustering")
-Sys.time()
+
 #build graph with k values of 10 and 50
-snn_k_10 <- buildSNNGraph(sce, k = 10, use.dimred = "HARMONY",type="jaccard")
-snn_k_50 <- buildSNNGraph(sce, k = 50, use.dimred = "HARMONY",type="jaccard")  
+print("Graph with k=20")
+Sys.time()
+snn_k_20 <- buildSNNGraph(sce, k = 20, use.dimred = "HARMONY")
 
-#Louvain clustering
-#Try different resolution values as well. Lower resolution values result in larger, fewer clusters
-#First K=10
-set.seed(1234)
-#1
-clust_10_1 <- igraph::cluster_louvain(snn_k_10,resolution=1)$membership
-print("k=10, louvain res = 1")
-table(clust_10_1)
-sce$k_10_louvain_1 <- factor(clust_10_1)
-k_10_1_tSNE <- plotReducedDim(object = sce,
+####Also run walktrap clustering 
+#k=20
+print("Running walktrap clustering with k = 20")
+Sys.time()
+
+wt_clusters_k_20 <- igraph::cluster_walktrap(snn_k_20)$membership
+
+print("k=20 done")
+Sys.time()
+
+#How many nuclei in each cluster?
+table(wt_clusters_k_20)
+
+#Add cluster information to the object
+sce$k_20_walktrap <- factor(wt_clusters_k_20)
+
+#tSNE with cluster information
+k_20_wt_tSNE <- plotReducedDim(object = sce,
                               dimred = "tSNE_HARMONY",
-                              colour_by = "k_10_louvain_1",
-                              text_by = "k_10_louvain_1") +
-  ggtitle("k=10 louvain clustering\nresolution=1") +
+                              colour_by = "k_20_walktrap",
+                              text_by = "k_20_walktrap") +
+  ggtitle("k=20 walktrap clustering") +
   theme(plot.title = element_text(hjust = 0.5))
-ggsave(plot = k_10_1_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_10_louvain_res1_tSNE.png"))
+ggsave(plot = k_20_wt_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_20_walktrap_tSNE.png"))
 
-#0.75
-clust_10.75 <- igraph::cluster_louvain(snn_k_10,resolution=0.75)$membership
-print("k=10, louvain res = .75")
-table(clust_10.75)
-sce$k_10_louvain_pt75 <- factor(clust_10.75)
-k_10_pt75_tSNE <- plotReducedDim(object = sce,
-                            dimred = "tSNE_HARMONY",
-                            colour_by = "k_10_louvain_pt75",
-                            text_by = "k_10_louvain_pt75") +
-  ggtitle("k=10 louvain clustering\nresolution=0.75") +
+
+#UMAP with cluster information
+k_20_wt_umap <- plotReducedDim(object = sce,
+                              dimred = "umap_HARMONY",
+                              colour_by = "k_20_walktrap",
+                              text_by = "k_20_walktrap") +
+  ggtitle("k=20 walktrap clustering") +
   theme(plot.title = element_text(hjust = 0.5))
-ggsave(plot = k_10_pt75_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_10_louvain_respt75_tSNE.png"))
-
-#0.5
-clust_10.5 <- igraph::cluster_louvain(snn_k_10,resolution=0.5)$membership
-print("k=10, louvain res = .5")
-table(clust_10.5)
-sce$k_10_louvain_pt5 <- factor(clust_10.5)
-k_10_pt5_tSNE <- plotReducedDim(object = sce,
-                                dimred = "tSNE_HARMONY",
-                                colour_by = "k_10_louvain_pt5",
-                                text_by = "k_10_louvain_pt5") +
-  ggtitle("k=10 louvain clustering\nresolution=0.5") +
-  theme(plot.title = element_text(hjust = 0.5))
-ggsave(plot = k_10_pt5_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_10_louvain_respt5_tSNE.png"))
-
-#0.25
-clust_10.25 <- igraph::cluster_louvain(snn_k_10,resolution=0.25)$membership
-sce$k_10_louvain_pt25 <- factor(clust_10.25)
-table(clust_10.25)
-k_10_pt25_tSNE <- plotReducedDim(object = sce,
-                                 dimred = "tSNE_HARMONY",
-                                 colour_by = "k_10_louvain_pt25",
-                                 text_by = "k_10_louvain_pt25") +
-  ggtitle("k=10 louvain clustering\nresolution=0.25") +
-  theme(plot.title = element_text(hjust = 0.5))
-ggsave(plot = k_10_pt25_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_10_louvain_respt25_tSNE.png"))
-
-#Now k=50
-set.seed(1234)
-#1
-clust_50_1 <- igraph::cluster_louvain(snn_k_50,resolution=1)$membership
-print("k=50, louvain res = 1")
-table(clust_50_1)
-sce$k_50_louvain_1 <- factor(clust_50_1)
-k_50_1_tSNE <- plotReducedDim(object = sce,
-                              dimred = "tSNE_HARMONY",
-                              colour_by = "k_50_louvain_1",
-                              text_by = "k_50_louvain_1") +
-  ggtitle("k=50 louvain clustering\nresolution=1") +
-  theme(plot.title = element_text(hjust = 0.5))
-ggsave(plot = k_50_1_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_50_louvain_res1_tSNE.png"))
-
-#0.75
-clust_50.75 <- igraph::cluster_louvain(snn_k_50,resolution=0.75)$membership
-print("k=50, louvain res = .75")
-table(clust_50.75)
-sce$k_50_louvain_pt75 <- factor(clust_50.75)
-k_50_pt75_tSNE <- plotReducedDim(object = sce,
-                            dimred = "tSNE_HARMONY",
-                            colour_by = "k_50_louvain_pt75",
-                            text_by = "k_50_louvain_pt75") +
-  ggtitle("k=50 louvain clustering\nresolution=0.75") +
-  theme(plot.title = element_text(hjust = 0.5))
-ggsave(plot = k_50_pt75_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_50_louvain_respt75_tSNE.png"))
-
-#0.5
-clust_50.5 <- igraph::cluster_louvain(snn_k_50,resolution=0.5)$membership
-print("k=50, louvain res = .5")
-table(clust_50.5)
-sce$k_50_louvain_pt5 <- factor(clust_50.5)
-k_50_pt5_tSNE <- plotReducedDim(object = sce,
-                                dimred = "tSNE_HARMONY",
-                                colour_by = "k_50_louvain_pt5",
-                                text_by = "k_50_louvain_pt5") +
-  ggtitle("k=50 louvain clustering\nresolution=0.5") +
-  theme(plot.title = element_text(hjust = 0.5))
-ggsave(plot = k_50_pt5_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_50_louvain_respt5_tSNE.png"))
-
-#0.25
-clust_50.25 <- igraph::cluster_louvain(snn_k_50,resolution=0.25)$membership
-sce$k_50_louvain_pt25 <- factor(clust_50.25)
-table(clust_50.25)
-k_50_pt25_tSNE <- plotReducedDim(object = sce,
-                                 dimred = "tSNE_HARMONY",
-                                 colour_by = "k_50_louvain_pt25",
-                                 text_by = "k_50_louvain_pt25") +
-  ggtitle("k=50 louvain clustering\nresolution=0.25") +
-  theme(plot.title = element_text(hjust = 0.5))
-ggsave(plot = k_50_pt25_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_50_louvain_respt25_tSNE.png"))
-
+ggsave(plot = k_20_wt_umap, filename = here("plots","12_snRNA","Dim_Red","k_20_walktrap_umap.png"))
 
 #Plot tSNE by sample
 for(i in unique(sce$Sample)){
@@ -157,15 +84,16 @@ for(i in unique(sce$Sample)){
                                              paste0(i,".png")))
 }
 
+
 print("Clustering complete")
+Sys.time()
 #######################################
 
-#Calcualte log counts and check expression of known marker genes to aid in cluster annotation
-#use k=10, 0.25 resolution for log-normalization. Will have to recalculate if choose different clustering. 
 print("Calculating log-normalized counts")
 Sys.time()
-sce <- computeSumFactors(sce,cluster = sce$k_10_louvain_pt25,min.mean = 0.1)
+sce <- computeSumFactors(sce,cluster = sce$k_20_walktrap,min.mean = 0.1)
 sce <- logNormCounts(sce)
+Sys.time()
 
 #Plot expression of known marker genes to be able to identify appropriate clusters
 genes <- c("SNAP25","SYT1","RBFOX3", #PAN-NEURON markers
@@ -176,6 +104,7 @@ genes <- c("SNAP25","SYT1","RBFOX3", #PAN-NEURON markers
            "DRD1","PDYN","EBF1","RXFP1","TAC1","CRHR2","RXFP1","CPNE4",#D1 markers
            "DRD2","PENK","ADORA2A",#D2 markers,
            "GRM8","CHST9","OPRM1", #D1-ISLANDS
+	   "DRD3","KCNT2","NTN1","TRPM3","PLD5", #ICJ
            "KIT","CHAT","SST", #INTERNEURON MARKERS
            "MOBP","MBP","OPALIN", #Oligodendrocytes
            "PDGFRA", #POLYDENDROCYTES
@@ -183,6 +112,7 @@ genes <- c("SNAP25","SYT1","RBFOX3", #PAN-NEURON markers
            "CD74", #Microglia
            "TAC3","GLP1R") #Misc. markers
 
+#tSNE
 for(i in genes){
   print(i)
   x <- plotReducedDim(object = sce,
@@ -193,8 +123,23 @@ for(i in genes){
   ggtitle(i) +
   theme(plot.title = element_text(hjust = 0.5))
   ggsave(plot = x,
-         filename = here("plots","12_snRNA","Expression","Known_Marker_Genes",
-                         paste0(i,".png")))
+         filename = here("plots","12_snRNA","Expression","Known_Marker_Genes","tSNE",
+                         paste0(i,"_tSNE.png")))
+}
+
+#umap
+for(i in genes){
+  print(i)
+  x <- plotReducedDim(object = sce,
+                      dimred = "umap_HARMONY",
+                      colour_by = i,
+                      swap_rownames = "gene_name") +
+  scale_color_gradientn(colours = c("lightgrey","red")) +
+  ggtitle(i) +
+  theme(plot.title = element_text(hjust = 0.5))
+  ggsave(plot = x,
+         filename = here("plots","12_snRNA","Expression","Known_Marker_Genes","UMAP",
+                         paste0(i,"_UMAP.png")))
 }
 
 #Plot expression of x and y linked genes. Color by Sample
@@ -207,15 +152,14 @@ x_y_linked <- plotExpression(object = sce,
   theme(axis.text.x = element_text(angle = 45,hjust = 1))
 ggsave(plot = x_y_linked,
        filename = here("plots","12_snRNA","Expression","X_Y_genes.png"))
+#######################################
 
 #Save the object
 print("Saving sce object")
-saveHDF5SummarizedExperiment(sce,
-                             here("processed-data","12_snRNA",
-                                  "sce_clustered"),
-                             replace = TRUE)
-print("Object saved")
+Sys.time()
+saveRDS(sce,file = here("processed-data","12_snRNA","sce_clustered_log.Rds"))
 
+print("Object saved")
 
 print("Reproducibility information:")
 Sys.time()
