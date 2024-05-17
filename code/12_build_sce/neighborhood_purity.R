16caa73b292cb0fd8bf2a8b814d6b8977d4b8097
Rphillips1995 <Rphillips11995@gmail.com>
2024-05-17 12:45:48 -0400

    assess whether neighborhood purity will aid in merging clusters

---
+++ code/12_build_sce/neighborhood_purity.R
@@ -0,0 +1,37 @@
+# cd /dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/
+# module load r_nac
+
+library(SingleCellExperiment)
+library(pheatmap)
+library(bluster)
+library(scater)
+library(scran)
+library(here)
+
+#Load in the object
+print("Loading Object")
+Sys.time()
+sce <- readRDS(here("processed-data","12_snRNA","sce_clustered_log.Rds"))
+print("Object loaded")
+Sys.time()
+
+sce
+
+#calculate purity 
+pure.sce <- neighborPurity(reducedDim(sce, "HARMONY"), sce$k_20_walktrap)
+
+#configure dataframe
+pure.data <- as.data.frame(pure.sce)
+pure.data$maximum <- factor(pure.data$maximum)
+pure.data$cluster <- sce$k_20_walktrap
+
+purity_plot <- ggplot(pure.data, aes(x=cluster, y=purity, colour=maximum)) +
+                ggbeeswarm::geom_quasirandom(method="smiley")
+
+ggsave(plot = purity_plot,file = here("plots","12_snRNA","Dim_Red","Purity_beeswarm.png"),height = 12, width = 12)
+
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
