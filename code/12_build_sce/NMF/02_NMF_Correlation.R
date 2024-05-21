b672b9b96399119a50d5c0aee74797388777a506
Rphillips1995 <Rphillips11995@gmail.com>
2024-05-21 17:16:24 -0400

    make csv with top 50 genes by nmf patterns + add nuclei weights to sce

--- code/12_build_sce/NMF/02_NMF_Correlation.R
+++ code/12_build_sce/NMF/02_NMF_Correlation.R
@@ -1,7 +1,7 @@
 #Completed in interactive job: srun --cpus-per-task=2 --mem=20G --pty --x11 bash
 #cd cd /dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc
 #module load r_nac
-
+#code modified from: https://github.com/LieberInstitute/spatialdACC/tree/main/code/snRNA-seq/06_NMF
 
 library(SingleCellExperiment)
 library(pheatmap)
@@ -241,3 +241,185 @@ dev.off()
 
 
 
+#Pull top 50 genes for each NMF pattern
+#Make w (feature factor matrix) a separate dataframe
+w_df <- as.data.frame(nmf_res@w)
+
+#Make an ensemble column and then merge with gene name info
+w_df$ensembl <- rownames(w_df)
+w_df_merge <- merge(x = w_df,
+                    y = rowData(sce)[,c("gene_id","gene_name")],
+                    by.x = "ensembl",
+                    by.y = "gene_id")
+
+#Change rownames to the gene_name
+rownames(w_df_merge) <- w_df_merge$gene_name
+
+#Keep only nmf patterns
+w_df_merge <- as.matrix(w_df_merge[,c(2:67)])
+ 
+# function for getting top n genes for each pattern
+top_genes <- function(W, n=50){
+  top_genes <- apply(W, 2, function(x) names(sort(x, decreasing=TRUE)[1:n]))
+  return(top_genes)
+}
+
+# get top 10 genes
+top50 <- top_genes(w_df_merge, 50)
+write.csv(top50, 
+          file = here("processed-data","12_snRNA","NMF", "NMF_top50_genes.csv"),
+          quote = FALSE)
+
+#lots of the top 10 features in each nmf pattern. 
+library(scater)
+library(ggplot2)
+
+load(here("processed-data","12_snRNA","CellType_Final_Cluster_Cols.rda"),verbose = TRUE)
+# Loading objects:
+#   cluster_cols
+
+#just to see, check top 10 genes for nmf27 which corresponds to one of the D1-island clusters
+plotExpression(object = sce,features = top50[1:10,"nmf27"],
+               x = "CellType.Final",swap_rownames = "gene_name",
+               color_by = "CellType.Final",ncol = 2) +
+  scale_color_manual(values = cluster_cols) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "none")
+
+##Add nmf patterns to sce object
+loads <- t(nmf_res@h)
+
+#Check that they are in the same order
+all(rownames(loads) == rownames(colData(sce)))
+#[1] TRUE
+
+#Add the patterns 
+colData(sce) <- cbind(colData(sce),loads)
+
+for(i in 1:66){
+  print(i)
+  x <- ggplot(data = as.data.frame(colData(sce)),
+              aes_string(x = "CellType.Final",
+                         y=paste0("nmf",i),
+                         fill = "CellType.Final")) +
+    geom_boxplot() +
+    scale_fill_manual(values = cluster_cols) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1),
+          legend.position = "none") 
+  ggsave(plot = x,
+         filename = here("plots","12_snRNA","NMF","nmf_celltype_vlns",
+                         paste0("nmf",i,".png")),
+         height = 6, 
+         width = 8)
+}
+
+#session info
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
+# [1] "Reproducibility information:"
+# [1] "2024-05-21 17:14:19 EDT"
+# user    system   elapsed 
+# 3917.374    15.946 14740.936 
+# ─ Session info ─────────────────────────────────────────────────────────────────────────
+# setting  value
+# version  R version 4.3.2 (2023-10-31)
+# os       Rocky Linux 9.2 (Blue Onyx)
+# system   x86_64, linux-gnu
+# ui       X11
+# language (EN)
+# collate  en_US.UTF-8
+# ctype    en_US.UTF-8
+# tz       US/Eastern
+# date     2024-05-21
+# pandoc   3.1.3 @ /jhpce/shared/libd/core/r_nac/1.0/nac_env/bin/pandoc
+# 
+# ─ Packages ─────────────────────────────────────────────────────────────────────────────
+# package              * version   date (UTC) lib source
+# abind                  1.4-5     2016-07-21 [1] CRAN (R 4.3.2)
+# beachmat               2.18.0    2023-10-24 [1] Bioconductor
+# beeswarm               0.4.0     2021-06-01 [1] CRAN (R 4.3.2)
+# Biobase              * 2.62.0    2023-10-24 [1] Bioconductor
+# BiocGenerics         * 0.48.1    2023-11-01 [1] Bioconductor
+# BiocNeighbors          1.20.2    2024-01-07 [1] Bioconductor 3.18 (R 4.3.2)
+# BiocParallel           1.36.0    2023-10-24 [1] Bioconductor
+# BiocSingular           1.18.0    2023-10-24 [1] Bioconductor
+# bitops                 1.0-7     2021-04-24 [1] CRAN (R 4.3.2)
+# cli                    3.6.2     2023-12-11 [1] CRAN (R 4.3.2)
+# codetools              0.2-19    2023-02-01 [1] CRAN (R 4.3.0)
+# colorspace             2.1-0     2023-01-23 [1] CRAN (R 4.3.0)
+# cowplot                1.1.1     2020-12-30 [1] CRAN (R 4.3.2)
+# crayon                 1.5.2     2022-09-29 [1] CRAN (R 4.3.0)
+# DelayedArray           0.28.0    2023-10-24 [1] Bioconductor
+# DelayedMatrixStats     1.24.0    2023-10-24 [1] Bioconductor
+# dplyr                  1.1.4     2023-11-17 [1] CRAN (R 4.3.2)
+# fansi                  1.0.6     2023-12-08 [1] CRAN (R 4.3.2)
+# farver                 2.1.1     2022-07-06 [1] CRAN (R 4.3.0)
+# generics               0.1.3     2022-07-05 [1] CRAN (R 4.3.0)
+# GenomeInfoDb         * 1.38.1    2023-11-08 [1] Bioconductor
+# GenomeInfoDbData       1.2.11    2023-12-12 [1] Bioconductor
+# GenomicRanges        * 1.54.1    2023-10-29 [1] Bioconductor
+# ggbeeswarm             0.7.2     2023-04-29 [1] CRAN (R 4.3.2)
+# ggplot2              * 3.4.4     2023-10-12 [1] CRAN (R 4.3.2)
+# ggrepel                0.9.4     2023-10-13 [1] CRAN (R 4.3.2)
+# glue                   1.7.0     2024-01-09 [1] CRAN (R 4.3.2)
+# gridExtra              2.3       2017-09-09 [1] CRAN (R 4.3.2)
+# gtable                 0.3.4     2023-08-21 [1] CRAN (R 4.3.1)
+# here                 * 1.0.1     2020-12-13 [1] CRAN (R 4.3.2)
+# IRanges              * 2.36.0    2023-10-24 [1] Bioconductor
+# irlba                  2.3.5.1   2022-10-03 [1] CRAN (R 4.3.2)
+# labeling               0.4.3     2023-08-29 [1] CRAN (R 4.3.1)
+# lattice                0.22-5    2023-10-24 [1] CRAN (R 4.3.1)
+# lifecycle              1.0.4     2023-11-07 [1] CRAN (R 4.3.2)
+# magrittr               2.0.3     2022-03-30 [1] CRAN (R 4.3.0)
+# Matrix                 1.6-4     2023-11-30 [1] CRAN (R 4.3.2)
+# MatrixGenerics       * 1.14.0    2023-10-24 [1] Bioconductor
+# matrixStats          * 1.2.0     2023-12-11 [1] CRAN (R 4.3.2)
+# munsell                0.5.0     2018-06-12 [1] CRAN (R 4.3.0)
+# pheatmap             * 1.0.12    2019-01-04 [1] CRAN (R 4.3.2)
+# pillar                 1.9.0     2023-03-22 [1] CRAN (R 4.3.0)
+# pkgconfig              2.0.3     2019-09-22 [1] CRAN (R 4.3.0)
+# plyr                   1.8.9     2023-10-02 [1] CRAN (R 4.3.1)
+# R6                     2.5.1     2021-08-19 [1] CRAN (R 4.3.0)
+# ragg                   1.2.7     2023-12-11 [1] CRAN (R 4.3.2)
+# RColorBrewer           1.1-3     2022-04-03 [1] CRAN (R 4.3.0)
+# Rcpp                   1.0.12    2024-01-09 [1] CRAN (R 4.3.2)
+# RcppML               * 0.3.7     2021-09-21 [1] CRAN (R 4.3.2)
+# RCurl                  1.98-1.13 2023-11-02 [1] CRAN (R 4.3.2)
+# reshape2             * 1.4.4     2020-04-09 [1] CRAN (R 4.3.0)
+# rlang                  1.1.3     2024-01-10 [1] CRAN (R 4.3.2)
+# rprojroot              2.0.4     2023-11-05 [1] CRAN (R 4.3.2)
+# rsvd                   1.0.5     2021-04-16 [1] CRAN (R 4.3.2)
+# S4Arrays               1.2.0     2023-10-24 [1] Bioconductor
+# S4Vectors            * 0.40.2    2023-11-23 [1] Bioconductor 3.18 (R 4.3.2)
+# ScaledMatrix           1.10.0    2023-10-24 [1] Bioconductor
+# scales                 1.3.0     2023-11-28 [1] CRAN (R 4.3.2)
+# scater               * 1.30.1    2023-11-16 [1] Bioconductor
+# scuttle              * 1.12.0    2023-10-24 [1] Bioconductor
+# sessioninfo            1.2.2     2021-12-06 [1] CRAN (R 4.3.2)
+# SingleCellExperiment * 1.24.0    2023-10-24 [1] Bioconductor
+# SparseArray            1.2.2     2023-11-07 [1] Bioconductor
+# sparseMatrixStats      1.14.0    2023-10-24 [1] Bioconductor
+# stringi                1.8.3     2023-12-11 [1] CRAN (R 4.3.2)
+# stringr                1.5.1     2023-11-14 [1] CRAN (R 4.3.2)
+# SummarizedExperiment * 1.32.0    2023-10-24 [1] Bioconductor
+# systemfonts            1.0.5     2023-10-09 [1] CRAN (R 4.3.1)
+# textshaping            0.3.7     2023-10-09 [1] CRAN (R 4.3.1)
+# tibble                 3.2.1     2023-03-20 [1] CRAN (R 4.3.0)
+# tidyselect             1.2.0     2022-10-10 [1] CRAN (R 4.3.0)
+# utf8                   1.2.4     2023-10-22 [1] CRAN (R 4.3.1)
+# vctrs                  0.6.5     2023-12-01 [1] CRAN (R 4.3.2)
+# vipor                  0.4.5     2017-03-22 [1] CRAN (R 4.3.2)
+# viridis                0.6.4     2023-07-22 [1] CRAN (R 4.3.2)
+# viridisLite            0.4.2     2023-05-02 [1] CRAN (R 4.3.0)
+# withr                  2.5.2     2023-10-30 [1] CRAN (R 4.3.1)
+# XVector                0.42.0    2023-10-24 [1] Bioconductor
+# zlibbioc               1.48.0    2023-10-24 [1] Bioconductor
+# 
+# [1] /jhpce/shared/libd/core/r_nac/1.0/nac_env/lib/R/library
+# 
+# ────────────────────────────────────────────────────────────────────────────────────────
+# 
