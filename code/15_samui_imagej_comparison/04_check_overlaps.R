66c2b56dfbdcbe1749d3f37fbeb35189ecd79eeb
Nick-Eagles <nick.eagles@libd.org>
2024-06-04 09:52:55 -0400

    Merge data at overlapping spots; produce a plot summarizing agreement by final alignment step, value of k, and clustering method

--- code/15_samui_imagej_comparison/04_check_overlaps.R
+++ code/15_samui_imagej_comparison/04_check_overlaps.R
@@ -15,8 +15,13 @@ precast_cluster_path = here(
     'processed-data', '15_samui_imagej_comparison', 'precast_out',
     'PRECAST_k%s_%s.csv'
 )
+plot_dir = here('plots', '15_samui_imagej_comparison')
 final_steps = c('imagej', 'samui')
 k_values = c(2, 10, 19, 28)
+
+################################################################################
+#   Read in cluster assignments
+################################################################################
 
 message('Reading in cluster assignments from PRECAST and BayesSpace...')
 cluster_df_list = list()
@@ -56,9 +61,15 @@ for (this_k in k_values) {
 #   Combine into a single tibble
 cluster_df = do.call(rbind, cluster_df_list)
 
+################################################################################
+#   Read in SPE, add overlap info, and extract necessary colData
+################################################################################
+
 spe = loadHDF5SummarizedExperiment(spe_dir)
 spe$capture_area = spe$sample_id_original
 
+#   Add overlap info for both forms of array coordinates. Really should've done
+#   this in 01_build_spe.R...
 for (final_step in final_steps) {
     message(sprintf('Adding overlap info for final_step=%s...', final_step))
 
@@ -94,8 +105,14 @@ col_data = colData(spe) |>
         capture_area, key, donor
     )
 
-cluster_df = cluster_df |>
+################################################################################
+#   Merge and explore data at overlaps
+################################################################################
+
+overlap_df = cluster_df |>
     left_join(col_data, by = "key") |>
+    #   Only take the array coordinates and overlap info related to the final
+    #   step in each row
     mutate(
         array_row = ifelse(
             final_step == 'imagej', array_row_imagej, array_row_samui
@@ -105,8 +122,45 @@ cluster_df = cluster_df |>
         ),
         exclude_overlapping = ifelse(
             final_step == 'imagej', exclude_overlapping_imagej, exclude_overlapping_samui
+        ),
+        overlap_key = ifelse(
+            final_step == 'imagej', overlap_key_imagej, overlap_key_samui
         )
     ) |>
-    select(-matches('^array_(row|col)_(imagej|samui)$'))
+    select(
+        -c(
+            matches('^array_(row|col)_(imagej|samui)$'),
+            matches('^(exclude_overlapping|overlap_key)_(imagej|samui)$')
+        )
+    ) |>
+    #   Only take spots overlapping other spots   
+    filter(overlap_key != "") |>
+    #   Add cluster assignment of the overlapping spot
+    mutate(
+        overlap_cluster_assignment = cluster_assignment[match(overlap_key, key)]
+    ) |>
+    #   Remove duplicate pairs and uncommon cases where the overlapping spot
+    #   was dropped before clustering
+    filter(!(key %in% overlap_key), !is.na(overlap_cluster_assignment))
 
-stopifnot(!any(is.na(cluster_df)))
+stopifnot(!any(is.na(overlap_df)))
+
+p = overlap_df |>
+    group_by(final_step, k, method, donor) |>
+    summarize(
+        agreement = mean(cluster_assignment == overlap_cluster_assignment)
+    ) |>
+    ungroup() |>
+    ggplot(mapping = aes(x = k, y = agreement, color = final_step)) +
+        geom_boxplot(outlier.shape = NA) +
+        geom_jitter(width = 0.2) +
+        facet_wrap(~method) +
+        labs(
+            y = "Proportion agreement at overlaps",
+            color = "Final alignment step"
+        ) +
+        theme_bw(base_size = 15)
+
+pdf(file.path(plot_dir, 'all_overlaps.pdf'))
+print(p)
+dev.off()
