861bb3238addc656a3e8e8a2e90a00261e8ad0b9
Nick-Eagles <nick.eagles@libd.org>
2024-06-05 08:58:52 -0400

    Add a shell script to run the check-overlaps script

---
+++ code/15_samui_imagej_comparison/04_check_overlaps.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH --mem=10G
+#SBATCH --job-name=04_check_overlaps
+#SBATCH -c 1
+#SBATCH -t 4:00:00
+#SBATCH -o ../../processed-data/15_samui_imagej_comparison/logs/04_check_overlaps.log
+#SBATCH -e ../../processed-data/15_samui_imagej_comparison/logs/04_check_overlaps.log
+
+set -e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.4
+module list
+
+Rscript 04_check_overlaps.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using slurmjobs version 1.2.2
+## available from http://research.libd.org/slurmjobs/
