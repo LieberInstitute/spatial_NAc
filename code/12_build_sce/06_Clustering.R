6f03646ce4595226a3ea769d3a65e7ac901a7d8c
Rphillips1995 <Rphillips11995@gmail.com>
2024-08-06 15:58:49 -0400

    Add res=0.75 for each k

--- code/12_build_sce/06_Clustering.R
+++ code/12_build_sce/06_Clustering.R
@@ -45,6 +45,14 @@ louvain_clusters_k_5_pt5 <- igraph::cluster_louvain(snn_k_5,resolution = 0.5)$me
 table(louvain_clusters_k_5_pt5)
 Sys.time()
 
+#louvain clustering with resolution of 0.75
+print("Running louvain k=5, res 0.75")
+Sys.time()
+set.seed(5)
+louvain_clusters_k_5_pt75 <- igraph::cluster_louvain(snn_k_5,resolution = 0.75)$membership
+table(louvain_clusters_k_5_pt75)
+Sys.time()
+
 #louvain clustering with resolution of 1
 print("Running louvain k=5, res 1")
 Sys.time()
@@ -55,22 +63,32 @@ Sys.time()
 
 #Add cluster information to the object
 sce$k_5_louvain_pt5 <- factor(louvain_clusters_k_5_pt5)
+sce$k_5_louvain_pt75 <- factor(louvain_clusters_k_5_pt75)
 sce$k_5_louvain_1 <- factor(louvain_clusters_k_5_1)
 
 #tSNE with cluster information
 k_5_pt5_tSNE <- plotReducedDim(object = sce,
-                                dimred = "tSNE_HARMONY",
-                                colour_by = "k_5_louvain_pt5",
-                                text_by = "k_5_louvain_pt5") +
+                               dimred = "tSNE_HARMONY",
+                               colour_by = "k_5_louvain_pt5",
+                               text_by = "k_5_louvain_pt5") +
   ggtitle("k=5 louvain clustering, resolution = 0.5") +
   theme(plot.title = element_text(hjust = 0.5))
 ggsave(plot = k_5_pt5_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_5_louvain_pt5_tSNE.png"))
 
 #tSNE with cluster information
+k_5_pt75_tSNE <- plotReducedDim(object = sce,
+                               dimred = "tSNE_HARMONY",
+                               colour_by = "k_5_louvain_pt75",
+                               text_by = "k_5_louvain_pt75") +
+  ggtitle("k=5 louvain clustering, resolution = 0.75") +
+  theme(plot.title = element_text(hjust = 0.5))
+ggsave(plot = k_5_pt5_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_5_louvain_pt75_tSNE.png"))
+
+#tSNE with cluster information
 k_5_1_tSNE <- plotReducedDim(object = sce,
-                              dimred = "tSNE_HARMONY",
-                              colour_by = "k_5_louvain_1",
-                              text_by = "k_5_louvain_1") +
+                             dimred = "tSNE_HARMONY",
+                             colour_by = "k_5_louvain_1",
+                             text_by = "k_5_louvain_1") +
   ggtitle("k=5 louvain clustering, resolution = 1") +
   theme(plot.title = element_text(hjust = 0.5))
 ggsave(plot = k_5_1_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_5_louvain_1_tSNE.png"))
@@ -92,6 +110,14 @@ louvain_clusters_k_10_pt5 <- igraph::cluster_louvain(snn_k_10,resolution = 0.5)$
 table(louvain_clusters_k_10_pt5)
 Sys.time()
 
+#louvain clustering with resolution of 0.75
+print("Running louvain k=10, res 0.75")
+Sys.time()
+set.seed(10)
+louvain_clusters_k_10_pt75 <- igraph::cluster_louvain(snn_k_10,resolution = 0.75)$membership
+table(louvain_clusters_k_10_pt75)
+Sys.time()
+
 #louvain clustering with resolution of 1
 print("Running louvain k=10, res 1")
 Sys.time()
@@ -102,6 +128,7 @@ Sys.time()
 
 #Add cluster information to the object
 sce$k_10_louvain_pt5 <- factor(louvain_clusters_k_10_pt5)
+sce$k_10_louvain_pt75 <- factor(louvain_clusters_k_10_pt75)
 sce$k_10_louvain_1 <- factor(louvain_clusters_k_10_1)
 
 #tSNE with cluster information
@@ -112,6 +139,15 @@ k_10_pt5_tSNE <- plotReducedDim(object = sce,
   ggtitle("k=10 louvain clustering, resolution = 0.5") +
   theme(plot.title = element_text(hjust = 0.5))
 ggsave(plot = k_10_pt5_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_10_louvain_pt5_tSNE.png"))
+
+#tSNE with cluster information
+k_10_pt75_tSNE <- plotReducedDim(object = sce,
+                                dimred = "tSNE_HARMONY",
+                                colour_by = "k_10_louvain_pt75",
+                                text_by = "k_10_louvain_pt75") +
+  ggtitle("k=10 louvain clustering, resolution = 0.75") +
+  theme(plot.title = element_text(hjust = 0.5))
+ggsave(plot = k_10_pt75_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_10_louvain_pt75_tSNE.png"))
 
 #tSNE with cluster information
 k_10_1_tSNE <- plotReducedDim(object = sce,
@@ -138,6 +174,14 @@ louvain_clusters_k_20_pt5 <- igraph::cluster_louvain(snn_k_20,resolution = 0.5)$
 table(louvain_clusters_k_20_pt5)
 Sys.time()
 
+#louvain clustering with resolution of 0.75
+print("Running louvain k=20, res 0.75")
+Sys.time()
+set.seed(20)
+louvain_clusters_k_20_pt75 <- igraph::cluster_louvain(snn_k_20,resolution = 0.75)$membership
+table(louvain_clusters_k_20_pt75)
+Sys.time()
+
 #louvain clustering with resolution of 1
 print("Running louvain k=20, res 1")
 Sys.time()
@@ -148,6 +192,7 @@ Sys.time()
 
 #Add cluster information to the object
 sce$k_20_louvain_pt5 <- factor(louvain_clusters_k_20_pt5)
+sce$k_20_louvain_pt75 <- factor(louvain_clusters_k_20_pt75)
 sce$k_20_louvain_1 <- factor(louvain_clusters_k_20_1)
 
 #tSNE with cluster information
@@ -160,78 +205,28 @@ k_20_pt5_tSNE <- plotReducedDim(object = sce,
 ggsave(plot = k_20_pt5_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_20_louvain_pt5_tSNE.png"))
 
 #tSNE with cluster information
+k_20_pt75_tSNE <- plotReducedDim(object = sce,
+                                 dimred = "tSNE_HARMONY",
+                                 colour_by = "k_20_louvain_pt75",
+                                 text_by = "k_20_louvain_pt75") +
+  ggtitle("k=20 louvain clustering, resolution = 0.75") +
+  theme(plot.title = element_text(hjust = 0.5))
+ggsave(plot = k_20_pt75_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_20_louvain_pt75_tSNE.png"))
+
+#tSNE with cluster information
 k_20_1_tSNE <- plotReducedDim(object = sce,
-                                dimred = "tSNE_HARMONY",
-                                colour_by = "k_20_louvain_1",
-                                text_by = "k_20_louvain_1") +
+                              dimred = "tSNE_HARMONY",
+                              colour_by = "k_20_louvain_1",
+                              text_by = "k_20_louvain_1") +
   ggtitle("k=20 louvain clustering, resolution = 1") +
   theme(plot.title = element_text(hjust = 0.5))
 ggsave(plot = k_20_1_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_20_louvain_1_tSNE.png"))
-
-
-##############K=50####################
-#build graph with k value of 50
-print("Graph with k=50")
-Sys.time()
-set.seed(50)
-snn_k_50 <- buildSNNGraph(sce, k = 50, use.dimred = "HARMONY")
-
-#louvain clustering with resolution of 0.5
-print("Running louvain k=50, res 0.5")
-Sys.time()
-set.seed(50)
-louvain_clusters_k_50_pt5 <- igraph::cluster_louvain(snn_k_50,resolution = 0.5)$membership
-table(louvain_clusters_k_50_pt5)
-Sys.time()
-
-#louvain clustering with resolution of 1
-print("Running louvain k=50, res 1")
-Sys.time()
-set.seed(50)
-louvain_clusters_k_50_1 <- igraph::cluster_louvain(snn_k_50,resolution = 1)$membership
-table(louvain_clusters_k_50_1)
-Sys.time()
-
-#Add cluster information to the object
-sce$k_50_louvain_pt5 <- factor(louvain_clusters_k_50_pt5)
-sce$k_50_louvain_1 <- factor(louvain_clusters_k_50_1)
-
-#tSNE with cluster information
-k_50_pt5_tSNE <- plotReducedDim(object = sce,
-                                dimred = "tSNE_HARMONY",
-                                colour_by = "k_50_louvain_pt5",
-                                text_by = "k_50_louvain_pt5") +
-  ggtitle("k=50 louvain clustering, resolution = 0.5") +
-  theme(plot.title = element_text(hjust = 0.5))
-ggsave(plot = k_50_pt5_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_50_louvain_pt5_tSNE.png"))
-
-#tSNE with cluster information
-k_50_1_tSNE <- plotReducedDim(object = sce,
-                              dimred = "tSNE_HARMONY",
-                              colour_by = "k_50_louvain_1",
-                              text_by = "k_50_louvain_1") +
-  ggtitle("k=50 louvain clustering, resolution = 1") +
-  theme(plot.title = element_text(hjust = 0.5))
-ggsave(plot = k_50_1_tSNE, filename = here("plots","12_snRNA","Dim_Red","k_50_louvain_1_tSNE.png"))
-
-#Plot tSNE by sample
-for(i in unique(sce$Sample)){
-  print(i)
-  tSNE_sample <- plotReducedDim(object = sce[,sce$Sample == i],
-                                dimred = "tSNE_HARMONY",
-                                colour_by = "Sample") +
-    ggtitle(unique(colData(sce[,sce$Sample == i])[,"Sort"])) +
-    theme(plot.title = element_text(hjust = 0.5))
-  ggsave(plot = tSNE_sample, filename = here("plots","12_snRNA",
-                                             "Dim_Red","tSNE_Sample_Specific",
-                                             paste0(i,".png")))
-}
 
 print("Clustering complete")
 Sys.time()
 #######################################
 
-print("Calculating log-normalized counts") #Going to jsut use the k=10, louvain 1 clustering --> Will use this clustering to generate final cell types
+print("Calculating log-normalized counts") #Going to jsut use the k=10, louvain 1 clustering
 Sys.time()
 sce <- computeSumFactors(sce,cluster = sce$k_10_louvain_1,min.mean = 0.1)
 sce <- logNormCounts(sce)
@@ -269,31 +264,6 @@ for(i in genes){
   ggsave(plot = x,
          filename = here("plots","12_snRNA","Expression","Known_Marker_Genes","tSNE",
                          paste0(i,"_tSNE.png")))
-}
-
-
-#Violin plots by each clustering 
-for(i in genes){
-  for(l in c("k_5_louvain_pt5","k_5_louvain_1",
-             "k_10_louvain_pt5","k_10_louvain_1",
-             "k_20_louvain_pt5","k_20_louvain_1",
-             "k_50_louvain_pt5","k_50_louvain_1")){
-    y <- plotExpression(sce,
-                        x = l, #x-axis by clustering 
-                        features = i, #feature is one of genes listed above
-                        swap_rownames = "gene_name") +
-      theme(axis.text.x = element_text(angle=90,hjust = 1),
-            legend.position = "none") +
-      stat_summary(fun = median, 
-                   fun.min = median, 
-                   fun.max = median,
-                   geom = "crossbar", 
-                   width = 0.3) 
-    ggsave(plot = y,
-           filename = here("plots","12_snRNA","Expression",
-                           "Known_Marker_Genes","Violin_Plots",l,
-                           paste0(i,l,"_Violin.png")))
-  }
 }
 
 #Plot expression of x and y linked genes. Color by Sample
