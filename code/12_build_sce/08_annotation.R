fe4ceb7952041664d5b93a05d3a256e59124696a
Rphillips1995 <Rphillips11995@gmail.com>
2024-08-09 15:57:13 -0400

    Annotate celltypes post k=10 louvain clustering + update log

--- code/12_build_sce/08_annotation.R
+++ code/12_build_sce/08_annotation.R
@@ -1,9 +1,6 @@
 #cd /dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/
 #module load r_nac
 
-#cd /dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/
-#module load r_nac
-
 library(SingleCellExperiment)
 library(sessioninfo)
 library(ggplot2)
@@ -28,7 +25,7 @@ sce
 ########
 #Preannotation plots
 #Calculate cluster percentage by BrainID
-brain_by_cluster <- as.data.frame.matrix(table(sce$Brain_ID,sce$k_20_walktrap))
+brain_by_cluster <- as.data.frame.matrix(table(sce$Brain_ID,sce$k_10_louvain_1))
 
 #Calculate percentages 
 brain_by_cluster_pct <- sweep(brain_by_cluster,MARGIN = 2,colSums(brain_by_cluster),"/") * 100
@@ -45,49 +42,49 @@ names(brain_cols) <- unique(sce$Brain_ID)
 brain_cluster_bar <- ggplot(data = brain_by_cluster_pct_melt,aes(x = variable,y = value,fill = brain)) +
   geom_bar(position = "stack",stat = "identity") +
   scale_fill_manual(values = brain_cols) +
-  labs(x = "k=20 walktrap cluster",
+  labs(x = "k=10 louvain, res=1 cluster",
        y = "Percent") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 ggsave(plot = brain_cluster_bar,
-       filename = here("plots","12_snRNA","Dim_Red","BrainID_by_cluster_k_20_walktrap_preannotation.png"),
+       filename = here("plots","12_snRNA","Dim_Red","BrainID_by_cluster_k_10_louvain_1_preannotation.png"),
        height = 12,
        width = 12)
 
 #Number of cells barplot
-Num_Nucs <- table(sce$k_20_walktrap) %>% as.data.frame() %>% 
+Num_Nucs <- table(sce$k_10_louvain_1) %>% as.data.frame() %>% 
   ggplot(aes(x = Var1, y = Freq, fill = Var1)) +
   geom_bar(stat = "identity") +
   theme_bw() +
   theme(legend.position = "none",
         axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(x = "k=20 walktrap cluster",
+  labs(x = "k=10 louvain, res=1 cluster",
        y = "Number of Nuclei") 
 
 ggsave(plot = Num_Nucs,
-       filename = here("plots","12_snRNA","k_20_walktrap_NumberofNuclei_Bargraph.png"),
+       filename = here("plots","12_snRNA","k_10_louvain_1_NumberofNuclei_Bargraph.png"),
        height = 12,
        width = 12)
 
 #Number of cells barplot split by sort
-Num_Nucs_sort <- table(sce$k_20_walktrap,sce$Sort) %>% as.data.frame() %>% 
+Num_Nucs_sort <- table(sce$k_10_louvain_1,sce$Sort) %>% as.data.frame() %>% 
   ggplot(aes(x = Var1, y = Freq, fill = Var2)) +
   geom_bar(stat = "identity",position = "dodge") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(x = "k=20 walktrap cluster",
+  labs(x = "k=10 louvain, res=1 cluster",
        y = "Number of Nuclei") 
 ggsave(plot = Num_Nucs_sort,
-       filename = here("plots","12_snRNA","k_20_walktrap_NumberofNucleibySort_Bargraph.png"),
+       filename = here("plots","12_snRNA","k_10_louvain_1_NumberofNucleibySort_Bargraph.png"),
        height = 12,
        width = 12)
 
 #Number of genes detected by cluster
 detected_vln <- plotColData(object = sce,
                             y = "detected",
-                            x = "k_20_walktrap",
-                            colour_by = "k_20_walktrap") +
+                            x = "k_10_louvain_1",
+                            colour_by = "k_10_louvain_1") +
   ggtitle("Number of Detected Genes") +
   stat_summary(fun = median, 
                fun.min = median, 
@@ -98,15 +95,15 @@ detected_vln <- plotColData(object = sce,
         axis.text.x = element_text(angle = 45, hjust = 1),
         plot.title = element_text(hjust = 0.5)) 
 ggsave(plot = detected_vln,
-       filename = here("plots","12_snRNA","k_20_walktrap_detected.png"),
+       filename = here("plots","12_snRNA","k_10_louvain_1_detected.png"),
        height = 12,
        width = 12)
 
 #percent mito
 percent_mito_vln <- plotColData(object = sce,
                                 y = "subsets_Mito_percent",
-                                x = "k_20_walktrap",
-                                colour_by = "k_20_walktrap") +
+                                x = "k_10_louvain_1",
+                                colour_by = "k_10_louvain_1") +
   ggtitle("Percent mito") +
   stat_summary(fun = median, 
                fun.min = median, 
@@ -117,7 +114,7 @@ percent_mito_vln <- plotColData(object = sce,
         axis.text.x = element_text(angle = 45, hjust = 1),
         plot.title = element_text(hjust = 0.5)) 
 ggsave(plot = percent_mito_vln,
-       filename = here("plots","12_snRNA","k_20_walktrap_PercentMito.png"),
+       filename = here("plots","12_snRNA","k_10_louvain_1_PercentMito.png"),
        height = 12,
        width = 12)
 
@@ -125,20 +122,27 @@ ggsave(plot = percent_mito_vln,
 #Cluster Annotations
 #read in the annotation frame
 annotation_df <- read.csv(file = here("processed-data","12_snRNA",
-                                      "Annotation_dataframe.csv"),
+                                      "k_10_louvain_1_annotation.csv"),
                           header = TRUE)
 
+sce$CellType.Final <- annotation_df$CellType[match(sce$k_10_louvain_1,
+                                                     annotation_df$k_10_louvain_1)]
 
-sce$CellType.Final <- annotation_df$Annotation[match(sce$k_20_walktrap,
-                                                     annotation_df$Cluster)]
+#load the cluster colors 
+load(here("processed-data","12_snRNA","070724_21colors_celltypeFinal.rda"),verbose = TRUE)
 
+cluster_cols
 
-#Annotate the tSNE
-cluster_cols <- Polychrome::createPalette(length(unique(sce$CellType.Final)),
-                                          c("#D81B60", "#1E88E5","#FFC107","#009E73"))
 names(cluster_cols) <- unique(sce$CellType.Final)
 
-save(cluster_cols,file = here("processed-data","12_snRNA","CellType_Final_Cluster_Cols.rda"))
+names(cluster_cols)[14] <- "Neuro_Ambig" #make Neuron_ambig grey
+names(cluster_cols)[15] <- "DRD1_MSN_D" #Swtich the original neuron_ambig 
+#Switch microglia and DRD2_MSN_A
+names(cluster_cols)[3] <- "DRD2_MSN_A"
+names(cluster_cols)[5] <- "Microglia"
+
+#resave the cluster colors. 
+save(cluster_cols,file = here("processed-data","12_snRNA","070924_21colors_celltypeFinal.rda"))
 
 #tSNE with annotations
 annotated_tSNE <- plotReducedDim(object = sce,
@@ -149,16 +153,6 @@ annotated_tSNE <- plotReducedDim(object = sce,
   theme(legend.position = "none")
 ggsave(filename = here("plots","12_snRNA","tSNE_CellType_Final.png"),
        plot = annotated_tSNE,height = 12, width = 12)
-
-#umap with annotations
-annotated_umap <- plotReducedDim(object = sce,
-                                 dimred = "umap_HARMONY",
-                                 colour_by = "CellType.Final",
-                                 text_by = "CellType.Final") +
-  scale_color_manual(values = cluster_cols) +
-  theme(legend.position = "none")
-ggsave(filename = here("plots","12_snRNA","umap_CellType_Final.png"),
-       plot = annotated_umap,height = 12, width = 12)
 
 ##########################3
 #Post annotation plots
