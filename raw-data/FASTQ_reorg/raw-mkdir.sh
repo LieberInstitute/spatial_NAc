87de7ac3752d392a49fa30f751b49b3b00a0f7f8
Ryan Miller <9923988+RyAMiller@users.noreply.github.com>
2025-09-09 13:26:32 -0400

    Delete raw-data directory from gh-pages

--- raw-data/FASTQ_reorg/raw-mkdir.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --array=1-20
-
-SAMPLE=$(awk "NR==${SLURM_ARRAY_TASK_ID}" raw-mkdir.txt)
-
-mkdir -p ./logs/
-mkdir -p /dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/raw-data/FASTQ_reorg/${SAMPLE}/
-
-mv slurm-*.out ./logs
